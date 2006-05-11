282a13eba0de9bec1ff4f757460693f19da6d156
zeroskill <zeroskill@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2006-05-11 23:00:09 +0000

    added statistic gathering for FPS fixed TimerManager code to better handle windows environments finished Demo1

--- trunk/examples/Demo1/demo1.cpp
+++ trunk/examples/Demo1/demo1.cpp
@@ -14,24 +14,37 @@ public:
 	virtual void postRun(){}
 private:
 	OpenGUI::Widgets::TextLabel* mLabel;
+	OpenGUI::TimerPtr mTimer;
 };
 
 void Demo1App::preRun()
 {
+	
+
 	OpenGUI::XMLParser::LoadFromFile("demo1.xml");
 	OpenGUI::System::getSingleton().setGUISheet(
 		OpenGUI::System::getSingleton().getGUISheetByName("root")
 		);
-	/*mLabel = static_cast<OpenGUI::Widgets::TextLabel*>
+	mLabel = static_cast<OpenGUI::Widgets::TextLabel*>
 		( OpenGUI::System::getSingleton().createWidget("TextLabel","OpenGUI") );
+	OpenGUI::System::getSingleton().getGUISheetByName("root")->addChildElement(mLabel,"Msg");
+	mLabel->setFont("pecot");
+	mLabel->setFontSize(16);
+	mLabel->setAlignment_Horiz(OpenGUI::TextAlignment::ALIGN_RIGHT);
+	mLabel->setPos(0.0f,0.0f);
+	mLabel->setSize(1.0f,1.0f);
+	mLabel->setText("Press ESC to Quit");
 
-	
+	mLabel = static_cast<OpenGUI::Widgets::TextLabel*>
+		( OpenGUI::System::getSingleton().createWidget("TextLabel","OpenGUI") );
 	OpenGUI::System::getSingleton().getGUISheetByName("root")->addChildElement(mLabel,"FPS");
-		mLabel->setFont("pecot");
-		mLabel->setFontSize(14);
-		mLabel->setPos(0.5f,0.5f);
-		mLabel->setSize(1.0f,1.0f);
-		mLabel->setText("teest");*/
+	mLabel->setFont("pecot");
+	mLabel->setFontSize(16);
+	mLabel->setPos(0.0f,0.0f);
+	mLabel->setSize(1.0f,1.0f);
+	mLabel->setText("FPS On the way!");
+
+	mTimer = OpenGUI::TimerManager::getSingleton().getTimer();
 	
 }
 void Demo1App::perframeRun()
@@ -49,6 +62,13 @@ void Demo1App::perframeRun()
 
 		OpenGUI::Widgets::Tachometer* t = static_cast<OpenGUI::Widgets::Tachometer*>(w);
 		t->setNeedleValue((float) val);
+
+		if(mTimer->getMilliseconds() > 200){
+			mTimer->reset();
+			std::stringstream ss;
+			ss << "FPS: " << OpenGUI::System::getSingleton().statRenderFPS();
+			mLabel->setText(ss.str());
+		}
 	}
 }
 
@@ -60,7 +80,6 @@ int main( void )
 	app.run();
 
 	return 0;
-	
 }
 
 
