f7e1947f7aa5a5d1e11e84456efb18b3c3618bc3
zeroskill <zeroskill@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2006-08-01 04:37:32 +0000

    Completed inline documentation for Message structs. See #7

--- trunk/OpenGUI/OpenGUI_MouseButton.h
+++ trunk/OpenGUI/OpenGUI_MouseButton.h
@@ -4,9 +4,9 @@
 namespace OpenGUI{
 	//! This enum represents mouse buttons for use in functions and structs that require a mouse button identifier.
 	typedef enum {
-			MouseButtonLeft=1,
-			MouseButtonMiddle=2,
-			MouseButtonRight=3
+			MouseButtonLeft=1, //!< Left mouse button
+			MouseButtonMiddle=2, //!< Middle mouse button
+			MouseButtonRight=3 //!< Right mouse button
 	} MouseButton;
 };
 #endif
