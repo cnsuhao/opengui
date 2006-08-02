9719296459db574c7998a234d0159ab74b857679
zeroskill <zeroskill@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2006-08-02 07:43:09 +0000

    Build script modifications to allow automated building of SDKs for both vc7.1 and vc8

--- rollW32Release.bat
+++ rollW32Release.bat
@@ -8,11 +8,36 @@ echo * Make sure that OpenGUI.h has the correct version number set.
 echo * Update the NSIS script (win32sdkInstaller.nsi) to reflect the new ver number.
 echo .
 echo = Note: You need to have Ogre setup and compiled correctly for OgreFusion SDK =
-echo When you're done with all of that, press a key and come back in 10 minutes.
+echo When you're done with all of that, press a key and come back in an hour.
 pause
 
+
+echo .
+echo Building Source Release...
 start /WAIT buildSRC_W32.bat SKIP
-start /WAIT buildSDK_W32.bat SKIP
+echo DONE: Building Source Release
+
+
+echo .
+echo Preparing Ogre for VC 7.1...
+start /WAIT prepareOgre.bat VC71
+echo DONE: Preparing Ogre for VC 7.1
+
+echo .
+echo Building SDK Release (VC 7.1)...
+start /WAIT buildSDK_W32_VC71.bat SKIP
+echo DONE: Building SDK Release (VC 7.1)
+
+
+echo .
+echo Preparing Ogre for VC 8...
+start /WAIT prepareOgre.bat VC8
+echo DONE: Preparing Ogre for VC 8
+
+echo .
+echo Building SDK Release (VC 8)...
+start /WAIT buildSDK_W32_VC8.bat SKIP
+echo DONE: Building SDK Release (VC 8)
 
 set NSIS="C:\Program Files\NSIS\makensis.exe"
 %NSIS% win32sdkInstaller.nsi
@@ -22,6 +47,7 @@ echo .
 echo NOW YOU JUST NEED TO
 echo * Check the sdk build log for build errors.
 echo * Place the winSRC directory in a zip file with an appropriate name.
-echo * Upload the new zip file and the sdk installer to SF.net and publich the release.
-echo * Post a release article on the OpenGUI website (opengui.sf.net)
+echo * Upload the new zip file and the sdk installer to SF.net and publish the release.
+echo * Post a release announcement on the OpenGUI website (opengui.rightbracket.com)
+echo * Post a release announcement on the Ogre forums (www.ogre3d.org)
 pause
