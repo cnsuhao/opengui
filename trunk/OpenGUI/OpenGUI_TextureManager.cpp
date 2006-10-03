3f9f3fb95ee832920d081f63075350d2bea743eb
zeroskill <zeroskill@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2006-10-03 06:36:43 +0000

    Added logging to TextureManager

--- trunk/OpenGUI/OpenGUI_TextureManager.cpp
+++ trunk/OpenGUI/OpenGUI_TextureManager.cpp
@@ -1,6 +1,7 @@
 #include "OpenGUI_TextureManager.h"
 #include "OpenGUI_Renderer.h"
 #include "OpenGUI_Exception.h"
+#include "OpenGUI_LogSystem.h"
 
 namespace OpenGUI {
 	//############################################################################
@@ -18,20 +19,27 @@ namespace OpenGUI {
 	//############################################################################
 	//############################################################################
 	TextureManager::TextureManager( Renderer* renderer ) {
+		LogManager::SlogMsg( "INIT", OGLL_INFO2 ) << "Creating TextureManager" << Log::endlog;
 		mRenderer = renderer;
 	}
 	//############################################################################
 	TextureManager::~TextureManager() {
+		LogManager::SlogMsg( "SHUTDOWN", OGLL_INFO2 ) << "Destroying TextureManager" << Log::endlog;
 		destroyAllTextures();
 	}
 	//############################################################################
 	TexturePtr TextureManager::createTextureFromFile( const std::string& filename ) {
+		LogManager::SlogMsg( "TextureManager", OGLL_INFO2 ) << "Create Texture from File: " << filename << Log::endlog;
 		Texture* tex = mRenderer->createTextureFromFile( filename );
 		mTextureCPtrList.push_front( tex );
 		return TexturePtr( tex );
 	}
 	//############################################################################
 	TexturePtr TextureManager::createTextureFromTextureData( const std::string& name, TextureData* textureData ) {
+		LogManager::SlogMsg( "TextureManager", OGLL_INFO2 ) << "Create Texture from TextureData: "
+			<< name
+			<< " (0x" << (unsigned int) textureData << ")"
+			<< Log::endlog;
 		Texture* tex = mRenderer->createTextureFromTextureData( textureData );
 		tex->_setName( name );
 		mTextureCPtrList.push_front( tex );
@@ -39,17 +47,24 @@ namespace OpenGUI {
 	}
 	//############################################################################
 	void TextureManager::updateTextureFromTextureData( TexturePtr texturePtr, TextureData* textureData ) {
+		LogManager::SlogMsg( "TextureManager", OGLL_INFO2 ) << "Update Texture from TextureData: "
+			<< texturePtr->getName()
+			<< " (0x" << (unsigned int) textureData << ")"
+			<< Log::endlog;
+
 		Texture* tex;
 		tex = texturePtr.get();
 		mRenderer->updateTextureFromTextureData( tex, textureData );
 	}
 	//############################################################################
 	void TextureManager::destroyTexture( Texture* texturePtr ) {
+		LogManager::SlogMsg( "TextureManager", OGLL_INFO2 ) << "DestroyTexture: " << texturePtr->getName() << Log::endlog;
 		mTextureCPtrList.remove( texturePtr );
 		mRenderer->destroyTexture( texturePtr );
 	}
 	//############################################################################
 	void TextureManager::destroyAllTextures() {
+		LogManager::SlogMsg( "TextureManager", OGLL_INFO2 ) << "DestroyAllTextures..." << Log::endlog;
 		Texture* tex;
 		while ( mTextureCPtrList.size() > 0 ) {
 			tex = mTextureCPtrList.front();
