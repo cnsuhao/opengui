4d5a1a8a92aad77f3290b8e74ca97e5895a2427d
zeroskill <zeroskill@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2006-10-07 10:06:39 +0000

    Initial setup for Control class.

--- trunk/OpenGUI/OpenGUI_Control.cpp
+++ trunk/OpenGUI/OpenGUI_Control.cpp
@@ -1,18 +1,18 @@
 #include "OpenGUI_Control.h"
+#include "OpenGUI_Exception.h"
 
 namespace OpenGUI {
-	/*
 	//############################################################################
-	class Widget_Name_ObjectProperty : public ObjectProperty {
+	class Control_Left_ObjectProperty : public ObjectProperty {
 	public:
 		virtual const char* getAccessorName() {
-			return "Name";
+			return "Left";
 		}
 		//############################################################################
 		virtual void get( Object& objectRef, Value& valueOut ) {
 			try {
-				Widget &w = dynamic_cast<Widget &>( objectRef );
-				valueOut.setValue( w.getName() );
+				Control& c = dynamic_cast<Control&>( objectRef );
+				valueOut.setValue( c.getLeft() );
 			} catch ( std::bad_cast e ) {
 				OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
 			}
@@ -20,31 +20,213 @@ namespace OpenGUI {
 		//############################################################################
 		virtual void set( Object& objectRef, Value& valueIn ) {
 			try {
-				Widget &w = dynamic_cast<Widget &>( objectRef );
-				w.setName( valueIn.getValueAsString() );
+				Control& c = dynamic_cast<Control&>( objectRef );
+				c.setLeft( valueIn.getValueAsFloat() );
 			} catch ( std::bad_cast e ) {
 				OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
 			}
 		}
 		//############################################################################
 		virtual Value::ValueType getPropertyType() {
-			return Value::T_STRING;
+			return Value::T_FLOAT;
 		}
 	}
-	gWidget_Name_ObjectProperty;
+	gControl_Left_ObjectProperty;
+	//############################################################################
+	class Control_Top_ObjectProperty : public ObjectProperty {
+	public:
+		virtual const char* getAccessorName() {
+			return "Top";
+		}
+		//############################################################################
+		virtual void get( Object& objectRef, Value& valueOut ) {
+			try {
+				Control& c = dynamic_cast<Control&>( objectRef );
+				valueOut.setValue( c.getTop() );
+			} catch ( std::bad_cast e ) {
+				OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
+			}
+		}
+		//############################################################################
+		virtual void set( Object& objectRef, Value& valueIn ) {
+			try {
+				Control& c = dynamic_cast<Control&>( objectRef );
+				c.setTop( valueIn.getValueAsFloat() );
+			} catch ( std::bad_cast e ) {
+				OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
+			}
+		}
+		//############################################################################
+		virtual Value::ValueType getPropertyType() {
+			return Value::T_FLOAT;
+		}
+	}
+	gControl_Top_ObjectProperty;
+	//############################################################################
+	class Control_Width_ObjectProperty : public ObjectProperty {
+	public:
+		virtual const char* getAccessorName() {
+			return "Width";
+		}
+		//############################################################################
+		virtual void get( Object& objectRef, Value& valueOut ) {
+			try {
+				Control& c = dynamic_cast<Control&>( objectRef );
+				valueOut.setValue( c.getWidth() );
+			} catch ( std::bad_cast e ) {
+				OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
+			}
+		}
+		//############################################################################
+		virtual void set( Object& objectRef, Value& valueIn ) {
+			try {
+				Control& c = dynamic_cast<Control&>( objectRef );
+				c.setWidth( valueIn.getValueAsFloat() );
+			} catch ( std::bad_cast e ) {
+				OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
+			}
+		}
+		//############################################################################
+		virtual Value::ValueType getPropertyType() {
+			return Value::T_FLOAT;
+		}
+	}
+	gControl_Width_ObjectProperty;
+	//############################################################################
+	class Control_Height_ObjectProperty : public ObjectProperty {
+	public:
+		virtual const char* getAccessorName() {
+			return "Height";
+		}
+		//############################################################################
+		virtual void get( Object& objectRef, Value& valueOut ) {
+			try {
+				Control& c = dynamic_cast<Control&>( objectRef );
+				valueOut.setValue( c.getHeight() );
+			} catch ( std::bad_cast e ) {
+				OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
+			}
+		}
+		//############################################################################
+		virtual void set( Object& objectRef, Value& valueIn ) {
+			try {
+				Control& c = dynamic_cast<Control&>( objectRef );
+				c.setHeight( valueIn.getValueAsFloat() );
+			} catch ( std::bad_cast e ) {
+				OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
+			}
+		}
+		//############################################################################
+		virtual Value::ValueType getPropertyType() {
+			return Value::T_FLOAT;
+		}
+	}
+	gControl_Height_ObjectProperty;
 	//############################################################################
 	//############################################################################
-	*/
-
+	class Control_Position_ObjectProperty : public ObjectProperty {
+	public:
+		virtual const char* getAccessorName() {
+			return "Position";
+		}
+		//############################################################################
+		virtual void get( Object& objectRef, Value& valueOut ) {
+			try {
+				Control& c = dynamic_cast<Control&>( objectRef );
+				valueOut.setValue( c.getPosition() );
+			} catch ( std::bad_cast e ) {
+				OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
+			}
+		}
+		//############################################################################
+		virtual void set( Object& objectRef, Value& valueIn ) {
+			/* read-only property */
+		}
+		//############################################################################
+		virtual Value::ValueType getPropertyType() {
+			return Value::T_FVECTOR2;
+		}
+		//############################################################################
+		virtual bool getPermSettable(){
+			return false; //read-only property
+		}
+	}
+	gControl_Position_ObjectProperty;
+	//############################################################################
+	class Control_Size_ObjectProperty : public ObjectProperty {
+	public:
+		virtual const char* getAccessorName() {
+			return "Size";
+		}
+		//############################################################################
+		virtual void get( Object& objectRef, Value& valueOut ) {
+			try {
+				Control& c = dynamic_cast<Control&>( objectRef );
+				valueOut.setValue( c.getSize() );
+			} catch ( std::bad_cast e ) {
+				OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
+			}
+		}
+		//############################################################################
+		virtual void set( Object& objectRef, Value& valueIn ) {
+			/* read-only property */
+		}
+		//############################################################################
+		virtual Value::ValueType getPropertyType() {
+			return Value::T_FVECTOR2;
+		}
+		//############################################################################
+		virtual bool getPermSettable(){
+			return false; //read-only property
+		}
+	}
+	gControl_Size_ObjectProperty;
+	//############################################################################
+	class Control_Rect_ObjectProperty : public ObjectProperty {
+	public:
+		virtual const char* getAccessorName() {
+			return "Rect";
+		}
+		//############################################################################
+		virtual void get( Object& objectRef, Value& valueOut ) {
+			try {
+				Control& c = dynamic_cast<Control&>( objectRef );
+				valueOut.setValue( c.getRect() );
+			} catch ( std::bad_cast e ) {
+				OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
+			}
+		}
+		//############################################################################
+		virtual void set( Object& objectRef, Value& valueIn ) {
+			/* read-only property */
+		}
+		//############################################################################
+		virtual Value::ValueType getPropertyType() {
+			return Value::T_FRECT;
+		}
+		//############################################################################
+		virtual bool getPermSettable(){
+			return false; //read-only property
+		}
+	}
+	gControl_Rect_ObjectProperty;
+	//############################################################################
+	//############################################################################
 	class Control_ObjectAccessorList : public ObjectAccessorList {
 	public:
 		Control_ObjectAccessorList() {
-			//addAccessor( &gWidget_Name_ObjectProperty );
+			addAccessor( &gControl_Left_ObjectProperty );
+			addAccessor( &gControl_Top_ObjectProperty );
+			addAccessor( &gControl_Width_ObjectProperty );
+			addAccessor( &gControl_Height_ObjectProperty );
+
+			addAccessor( &gControl_Position_ObjectProperty );
+			addAccessor( &gControl_Size_ObjectProperty );
+			addAccessor( &gControl_Rect_ObjectProperty );
 		}
 		~Control_ObjectAccessorList() {}
 	}
 	gControl_ObjectAccessorList;
-
 	//############################################################################
 	//############################################################################
 
@@ -53,6 +235,17 @@ namespace OpenGUI {
 	Control::Control() {
 		if ( gControl_ObjectAccessorList.getParent() == 0 )
 			gControl_ObjectAccessorList.setParent( Widget::getAccessors() );
+
+		// set up defaults for properties
+		mRect = FRect( 0.0f, 0.0f, 1.0f, 1.0f );
+
+		//Set up events and default bindings
+		getEvents().createEvent("Cursor_Click");
+		getEvents().createEvent("Cursor_Enter");
+		getEvents().createEvent("Cursor_Leave");
+		getEvents()["Cursor_Click"].add( new EventDelegate(this, &Control::onCursor_Click) );
+		getEvents()["Cursor_Enter"].add( new EventDelegate(this, &Control::onCursor_Enter) );
+		getEvents()["Cursor_Leave"].add( new EventDelegate(this, &Control::onCursor_Leave) );
 	}
 	//############################################################################
 	Control::~Control() {
@@ -65,6 +258,81 @@ namespace OpenGUI {
 	//############################################################################
 	char* Control::getClassName() {
 		return "OpenGUI::Control";
+	}
+	//############################################################################
+	void Control::setLeft( float left ){
+		mRect.setPosition( FVector2(left, mRect.min.y) );
+	}
+	//############################################################################
+	float Control::getLeft(){
+		return mRect.min.x;
+	}
+	//############################################################################
+	void Control::setTop( float top ){
+		mRect.setPosition( FVector2(mRect.min.x, top) );
+	}
+	//############################################################################
+	float Control::getTop(){
+		return mRect.min.y;
+	}
+	//############################################################################
+	/*! Controls cannot have a width less than 0.0f, so negative values are clamped to 0.0f */
+	void Control::setWidth( float width ){
+		if( width < 0.0f ) width = 0.0f;
+		mRect.setWidth( width );
+	}
+	//############################################################################
+	float Control::getWidth(){
+		return mRect.getWidth();
+	}
+	//############################################################################
+	/*! Controls cannot have a height less than 0.0f, so negative values are clamped to 0.0f */
+	void Control::setHeight( float height ){
+		if( height < 0.0f ) height = 0.0f;
+		mRect.setHeight( height );
+	}
+	//############################################################################
+	float Control::getHeight(){
+		return mRect.getHeight();
+	}
+	//############################################################################
+	const FVector2& Control::getPosition(){
+		return mRect.min;
+	}
+	//############################################################################
+	FVector2 Control::getSize(){
+		return mRect.getSize();
+	}
+	//############################################################################
+	const FRect& Control::getRect(){
+		return mRect;
+	}
+	//############################################################################
+	void Control::onCursor_Click( Object* sender, EventArgs& evtArgs ){
+		/* Default is to do nothing */
+	}
+	//############################################################################
+	void Control::onCursor_Enter( Object* sender, EventArgs& evtArgs ){
+		/* Default is to do nothing */
+	}
+	//############################################################################
+	void Control::onCursor_Leave( Object* sender, EventArgs& evtArgs ){
+		/* Default is to do nothing */
+	}
+	//############################################################################
+	void Control::eventCursor_Click(){
+		EventArgs event;
+		getEvents()["Cursor_Click"].invoke( this, event );
+	}
+	//############################################################################
+	void Control::eventCursor_Enter(){
+		EventArgs event;
+		getEvents()["Cursor_Enter"].invoke( this, event );
+	}
+	//############################################################################
+	void Control::eventCursor_Leave(){
+		EventArgs event;
+		getEvents()["Cursor_Leave"].invoke( this, event );
 	}
 	//############################################################################
 } // namespace OpenGUI {
--- trunk/OpenGUI/OpenGUI_Control.h
+++ trunk/OpenGUI/OpenGUI_Control.h
@@ -10,6 +10,21 @@
 namespace OpenGUI {
 
 	//! Base class for all GUI objects that have position and size
+	/*!
+	\par Properties
+	- Left (setLeft, getLeft)
+	- Top (setTop, getTop)
+	- Width (setWidth, getWidth)
+	- Height (setHeight, getHeight)
+	- Position (getPosition)
+	- Size (getSize)
+	- Rect (getRect)
+
+	\par Events
+	- Cursor_Click (onCursor_Click, eventCursor_Click)
+	- Cursor_Enter (onCursor_Enter, eventCursor_Enter)
+	- Cursor_Leave (onCursor_Leave, eventCursor_Leave)
+	*/
 	class OPENGUI_API Control : public Widget {
 	public:
 		//! public constructor
@@ -17,21 +32,85 @@ namespace OpenGUI {
 		//! public destructor
 		virtual ~Control();
 
+		//! Sets the position of the left side of the Control
+		void setLeft( float left );
+		//! Returns the position of the left side of the Control
+		float getLeft();
+		//! Sets the position of the top side of the Control
+		void setTop( float top );
+		//! Returns the position of the top side of the Control
+		float getTop();
+		//! Sets the width of the Control
+		void setWidth( float width );
+		//! Returns the width of the Control
+		float getWidth();
+		//! Sets the height of the Control
+		void setHeight( float height );
+		//! Returns the height of the Control
+		float getHeight();
 
-		//! Sets the alpha transparency of this Widget
-		void setAlpha( float alpha );
-		//! Returns the alpha transparency of this Widget
-		float getAlpha();
-
-
+		//! Returns the position of the upper left corner of the Control
+		const FVector2& getPosition();
+		//! Returns the size of the Control
+		FVector2 getSize();
+		//! Returns the FRect describing this Control's size and position
+		const FRect& getRect();
 
 		//Object Functions
 		virtual ObjectAccessorList* getAccessors();
 		virtual char* getClassName();
 
 	protected:
+//!\name Event Handlers
+//@{
+		//! "Cursor_Click" event
+		virtual void onCursor_Click( Object* sender, EventArgs& evtArgs );
+		//! "Cursor_Enter" event
+		virtual void onCursor_Enter( Object* sender, EventArgs& evtArgs );
+		//! "Cursor_Leave" event
+		virtual void onCursor_Leave( Object* sender, EventArgs& evtArgs );
+//@}
+
+//!\name Event Processors
+//@{
+		//! Called when cursor was pressed and released within this Control
+		virtual void eventCursor_Click();
+		//! Called when the cursor enters this Control
+		virtual void eventCursor_Enter();
+		//! Called when the cursor leaves this Control
+		virtual void eventCursor_Leave();
+//@}
+
+		/*
+		Move_Begin
+		Moving
+		Move_End
+		Moved
+
+		Resize_Being
+		Resizing
+		Resize_End
+		Resized
+
+
+		Menu_Up
+		Menu_Down
+		Menu_Left
+		Menu_Right
+		Menu_Activate
+		Menu_Targeted
+		Menu_UnTargeted
+
+		Targeted
+		UnTargeted
+
+		Focus
+		Focus_Lost
+
+		Activated
+		*/
 	private:
-		float mAlpha;
+		FRect mRect; // position & size of this Control
 	};
 
 } // namespace OpenGUI{
