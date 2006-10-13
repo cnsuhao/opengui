46174aa1867a2f2c060fcaecea10c59c4937f584
zeroskill <zeroskill@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2006-10-13 10:18:18 +0000

    Initial layout code. Should work, but will need testing.

--- trunk/OpenGUI/OpenGUI_ContainerControl.cpp
+++ trunk/OpenGUI/OpenGUI_ContainerControl.cpp
@@ -89,22 +89,127 @@ namespace OpenGUI {
 		return m_LayoutValid;
 	}
 	//############################################################################
+	/*! This is virtual so that derived classes can redefine it to provide new client
+	area sizes and positions as desired.
+
+	The Client Area is the area of the ContainerControl that child widgets are drawn
+	within. The Client Area's position is an offset from this ContainerControl's
+	position. So a Client Area position of 0,0 would place the upper left of the
+	Client Area exactly in the upper left corner of this ContainerControl, regardless
+	of where this ContainerControl is actually placed on the Screen.
+
+	The default implementation always returns a client area that fully encompasses
+	the ContainerControl's drawing area.*/
+	const FRect& ContainerControl::getClientArea() {
+		static FRect tmp;
+		tmp.setSize( getSize() );
+		return tmp;
+	}
+	//############################################################################
 	/*! The update is performed regardless of layout being currently suspended,
 	so only call this if you really want the update to occur. Layout validity,
 	however, is still honored and layouts will not occur unless needed. */
 	void ContainerControl::updateLayout() {
 		m_InUpdateLayout = true;
 		if ( !layoutValid() ) {
+			_setChildControlLayoutState( true );
 			_doUpdateLayout();
 			m_LayoutValid = true;
+			_setChildControlLayoutState( false );
 		}
 		m_InUpdateLayout = false;
+	}
+	//############################################################################
+	void ContainerControl::_setChildControlLayoutState( bool state ) {
+		for ( WidgetCollection::iterator iter = Children.begin();
+				iter != Children.end(); iter++ ) {
+			Control* c = dynamic_cast<Control*>( iter.get() );
+			if ( c )
+				c->m_InLayout = state;
+		}
 	}
 	//############################################################################
 	/*! This is virtual in the case that a subclass wishes to perform their own layout logic.
 	Toolbars and other specialty containers would likely need this functionality. */
 	void ContainerControl::_doUpdateLayout() {
+		//store previous client area space
+		FRect oldClntArea = m_RemClntArea;
+		m_RemClntArea = getClientArea();
 
+		//update all docked controls
+		for ( WidgetCollection::iterator iter = Children.begin();
+				iter != Children.end(); iter++ ) {
+			Control* ctrl = dynamic_cast<Control*>( iter.get() );
+			if ( ctrl ) {
+				int dock = ctrl->getDocking();
+				if ( dock ) {
+
+					//Next two IF's cover position and height for 'Fill' as well
+					if (( dock & Control::Left ) || ( dock & Control::Right ) ) {
+						ctrl->setTop( m_RemClntArea.getPosition().y );
+						ctrl->setHeight( m_RemClntArea.getHeight() );
+					}
+					if (( dock & Control::Top ) || ( dock & Control::Bottom ) ) {
+						ctrl->setLeft( m_RemClntArea.getPosition().x );
+						ctrl->setWidth( m_RemClntArea.getWidth() );
+					}
+
+					if ( dock == Control::Left ) {
+						ctrl->setLeft( m_RemClntArea.getPosition().x );
+						m_RemClntArea.setWidth( m_RemClntArea.getWidth() - ctrl->getWidth() );
+						m_RemClntArea.offset( FVector2( ctrl->getWidth(), 0.0f ) );
+					}
+					if ( dock == Control::Right ) {
+						ctrl->setLeft( m_RemClntArea.getPosition().x + m_RemClntArea.getWidth() - ctrl->getWidth() );
+						m_RemClntArea.setWidth( m_RemClntArea.getWidth() - ctrl->getWidth() );
+					}
+
+					if ( dock == Control::Top ) {
+						ctrl->setTop( m_RemClntArea.getPosition().y );
+						m_RemClntArea.setHeight( m_RemClntArea.getHeight() - ctrl->getHeight() );
+						m_RemClntArea.offset( FVector2( 0.0f, ctrl->getHeight() ) );
+					}
+					if ( dock == Control::Bottom ) {
+						ctrl->setTop( m_RemClntArea.getPosition().y + m_RemClntArea.getHeight() - ctrl->getHeight() );
+						m_RemClntArea.setHeight( m_RemClntArea.getHeight() - ctrl->getHeight() );
+					}
+				}
+			}
+		}
+
+		//use difference of new and previous client area space to update anchored controls
+		float deltaLeft = m_RemClntArea.getPosition().x - oldClntArea.getPosition().x;
+		float deltaTop = m_RemClntArea.getPosition().y - oldClntArea.getPosition().y ;
+		float deltaRight = ( m_RemClntArea.getPosition().x + m_RemClntArea.getWidth() ) - ( oldClntArea.getPosition().x + oldClntArea.getWidth() ) ;
+		float deltaBottom = ( m_RemClntArea.getPosition().y + m_RemClntArea.getHeight() ) - ( oldClntArea.getPosition().y + oldClntArea.getHeight() ) ;
+
+		for ( WidgetCollection::iterator iter = Children.begin();
+				iter != Children.end(); iter++ ) {
+			Control* ctrl = dynamic_cast<Control*>( iter.get() );
+			if ( ctrl ) {
+				int dock = ctrl->getDocking();
+				if ( dock == Control::None ) {
+					int anchor = ctrl->getAnchor();
+					if(anchor & Control::Left){
+						ctrl->setLeft( ctrl->getLeft() + deltaLeft );
+						if(anchor & Control::Right)
+							ctrl->setWidth( (ctrl->getWidth() - deltaLeft) + deltaRight);
+					}
+					if( (anchor & Control::Left) == 0 && (anchor & Control::Right)){
+						ctrl->setLeft( ctrl->getLeft() + deltaRight );
+					}
+
+					if(anchor & Control::Top){
+						ctrl->setTop( ctrl->getTop() + deltaTop );
+						if(anchor & Control::Bottom)
+							ctrl->setHeight( (ctrl->getHeight() - deltaTop) + deltaBottom);
+					}
+					if( (anchor & Control::Top) == 0 && (anchor & Control::Bottom)){
+						ctrl->setTop( ctrl->getTop() + deltaBottom );
+					}
+				}
+			}
+		}
 	}
 	//############################################################################
 } // namespace OpenGUI {
