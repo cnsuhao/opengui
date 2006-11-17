a20ade48197eec3c8a17ec66a2b8aa850e90fba0
mmanyen <mmanyen@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2006-11-17 21:54:51 +0000

    Ticket: #103

---
+++ Amethyst/Amethyst_Frame.cpp
@@ -0,0 +1,116 @@
+#include "Amethyst_Frame.h"
+
+namespace OpenGUI {
+	namespace Amethyst {
+
+		Frame::Frame()
+		{
+			mFrameWidth = 5;
+			mFrameHeight = 5;
+		}
+
+		Frame::~Frame()
+		{
+		}
+
+
+		Widget* Frame::CreateFrameFactory()
+		{
+			return new Frame();
+		}
+
+		//! Sets the various frame imagery.
+		void Frame::setImagery( std::string imageryName, eFrameImageIndex indx)
+		{
+			invalidate();
+			mFramePieces[indx] = ImageryManager::getSingleton().getImagery( imageryName );
+		}
+
+		//! Gets the various frame imagery.
+		std::string Frame::getImagery(eFrameImageIndex indx)
+		{
+			if(mFramePieces[indx].isNull())
+				return "";
+
+			return mFramePieces[indx]->getName();
+		}
+
+
+
+		void Frame::onDraw( Object* sender, Draw_EventArgs& evtArgs )
+		{
+			Brush& b = evtArgs.brush;
+
+			if ( !mFramePieces[UL].isNull() ) {
+				FRect myRect = getRect();
+				myRect.setWidth(mFrameWidth);
+				myRect.setHeight(mFrameHeight);
+				b.Image.drawImage( mFramePieces[UL], myRect );
+			}
+			if ( !mFramePieces[UM].isNull() ) {
+				FRect myRect = getRect();
+				myRect.setPosition(myRect.getPosition() + FVector2(mFrameWidth, 0));
+				myRect.setWidth(getRect().getWidth() - (mFrameWidth*2));
+				myRect.setHeight(mFrameHeight);
+				b.Image.drawImage( mFramePieces[UM], myRect );
+			}
+			if ( !mFramePieces[UR].isNull() ) {
+				FRect myRect = getRect();
+				myRect.setPosition(myRect.getPosition() + FVector2(getRect().getWidth() - mFrameWidth, 0));
+				myRect.setWidth(mFrameWidth);
+				myRect.setHeight(mFrameHeight);
+				b.Image.drawImage( mFramePieces[UR], myRect );
+			}
+
+			if ( !mFramePieces[ML].isNull() ) {
+				FRect myRect = getRect();
+				myRect.setPosition(myRect.getPosition() + FVector2(0, mFrameHeight));
+				myRect.setWidth(mFrameWidth);
+				myRect.setHeight(getRect().getHeight() - (2*mFrameHeight));
+				b.Image.drawImage( mFramePieces[ML], myRect );
+			}
+			if ( !mFramePieces[MM].isNull() ) {
+				FRect myRect = getRect();
+				myRect.setPosition(myRect.getPosition() + FVector2(mFrameWidth, mFrameHeight));
+				myRect.setWidth(getRect().getWidth() - (mFrameWidth*2));
+				myRect.setHeight(getRect().getHeight() - (2*mFrameHeight));
+				b.Image.drawImage( mFramePieces[MM], myRect );
+			}
+			if ( !mFramePieces[MR].isNull() ) {
+				FRect myRect = getRect();
+				myRect.setPosition(myRect.getPosition() + FVector2(getRect().getWidth() - mFrameWidth, mFrameHeight));
+				myRect.setWidth(mFrameWidth);
+				myRect.setHeight(getRect().getHeight() - (2*mFrameHeight));
+				b.Image.drawImage( mFramePieces[MR], myRect );
+			}
+
+
+
+
+			if ( !mFramePieces[LL].isNull() ) {
+				FRect myRect = getRect();
+				myRect.setPosition(myRect.getPosition() + FVector2(0, getRect().getHeight() - mFrameHeight));
+				myRect.setWidth(mFrameWidth);
+				myRect.setHeight(mFrameHeight);
+				b.Image.drawImage( mFramePieces[LL], myRect );
+			}
+			if ( !mFramePieces[LM].isNull() ) {
+				FRect myRect = getRect();
+				myRect.setPosition(myRect.getPosition() + FVector2(mFrameWidth, getRect().getHeight() - mFrameHeight));
+				myRect.setWidth(getRect().getWidth() - (mFrameWidth*2));
+				myRect.setHeight(mFrameHeight);
+				b.Image.drawImage( mFramePieces[LM], myRect );
+			}
+			if ( !mFramePieces[LR].isNull() ) {
+				FRect myRect = getRect();
+				myRect.setPosition(myRect.getPosition() + FVector2(getRect().getWidth() - mFrameWidth, getRect().getHeight() - mFrameHeight));
+				myRect.setWidth(mFrameWidth);
+				myRect.setHeight(mFrameHeight);
+				b.Image.drawImage( mFramePieces[LR], myRect );
+			}
+		}
+
+
+	} // namespace Amethyst{
+} // namespace OpenGUI{
+
