5a587036f995a2439d3d422fd5b9d7bc3a630a56
zeroskill <zeroskill@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2006-11-23 01:20:44 +0000

    Visual Studio 2005 project files should be up to date Ticket: #108

--- trunk/Renderers/Ogre/DemoO/setupEnv_VC8.bat
+++ trunk/Renderers/Ogre/DemoO/setupEnv_VC8.bat
@@ -4,48 +4,42 @@ mkdir Release
 
 set OgreSDKdir=%OGRE_HOME_vc8%
 
-copy DemoO.zip Debug\
-copy DemoO.zip Release\
-copy resources.cfg Debug\
-copy resources.cfg Release\
-copy Plugins.cfg Debug\
-copy Plugins.cfg Release\
+copy DemoO.zip ..\bin\Debug\
+copy DemoO.zip ..\bin\Release\
+copy resources.cfg ..\bin\Debug\
+copy resources.cfg ..\bin\Release\
+copy Plugins.cfg ..\bin\Debug\
+copy Plugins.cfg ..\bin\Release\
 
-copy ..\..\Debug\OpenGUI_d.dll Debug\
-copy ..\..\Release\OpenGUI.dll Release\
-copy ..\..\Debug\Renderer_Ogre_d.dll Debug\
-copy ..\..\Release\Renderer_Ogre.dll Release\
-copy ..\bin\TachometerWidget_d.dll Debug\
-copy ..\bin\TachometerWidget.dll Release\
 
 rem Copy Ogre Debug
-copy %OgreSDKdir%\bin\debug\cg.dll Debug\
-copy %OgreSDKdir%\bin\debug\DevIL.dll Debug\
-copy %OgreSDKdir%\bin\debug\ILU.dll Debug\
-copy %OgreSDKdir%\bin\debug\ILUT.dll Debug\
-copy %OgreSDKdir%\bin\debug\zlib1.dll Debug\
-copy %OgreSDKdir%\bin\debug\OgreMain_d.dll Debug\
-copy %OgreSDKdir%\bin\debug\OgrePlatform_d.dll Debug\
-copy %OgreSDKdir%\bin\debug\Plugin_CgProgramManager.dll Debug\
-copy %OgreSDKdir%\bin\debug\Plugin_ParticleFX.dll Debug\
-copy %OgreSDKdir%\bin\debug\Plugin_OctreeSceneManager.dll Debug\
-copy %OgreSDKdir%\bin\debug\Plugin_BSPSceneManager.dll Debug\
-copy %OgreSDKdir%\bin\debug\RenderSystem_Direct3D9.dll Debug\
-copy %OgreSDKdir%\bin\debug\RenderSystem_GL.dll Debug\
-copy %OgreSDKdir%\media\packs\OgreCore.zip Debug\
+copy %OgreSDKdir%\bin\debug\cg.dll ..\bin\Debug\
+copy %OgreSDKdir%\bin\debug\DevIL.dll ..\bin\Debug\
+copy %OgreSDKdir%\bin\debug\ILU.dll ..\bin\Debug\
+copy %OgreSDKdir%\bin\debug\ILUT.dll ..\bin\Debug\
+copy %OgreSDKdir%\bin\debug\zlib1.dll ..\bin\Debug\
+copy %OgreSDKdir%\bin\debug\OgreMain_d.dll ..\bin\Debug\
+copy %OgreSDKdir%\bin\debug\OgrePlatform_d.dll ..\bin\Debug\
+copy %OgreSDKdir%\bin\debug\Plugin_CgProgramManager.dll ..\bin\Debug\
+copy %OgreSDKdir%\bin\debug\Plugin_ParticleFX.dll ..\bin\Debug\
+copy %OgreSDKdir%\bin\debug\Plugin_OctreeSceneManager.dll ..\bin\Debug\
+copy %OgreSDKdir%\bin\debug\Plugin_BSPSceneManager.dll ..\bin\Debug\
+copy %OgreSDKdir%\bin\debug\RenderSystem_Direct3D9.dll ..\bin\Debug\
+copy %OgreSDKdir%\bin\debug\RenderSystem_GL.dll ..\bin\Debug\
+copy %OgreSDKdir%\media\packs\OgreCore.zip ..\bin\Debug\
 
 rem Copy Ogre Release
-copy %OgreSDKdir%\bin\release\cg.dll Release\
-copy %OgreSDKdir%\bin\release\DevIL.dll Release\
-copy %OgreSDKdir%\bin\release\ILU.dll Release\
-copy %OgreSDKdir%\bin\release\ILUT.dll Release\
-copy %OgreSDKdir%\bin\release\zlib1.dll Release\
-copy %OgreSDKdir%\bin\release\OgreMain.dll Release\
-copy %OgreSDKdir%\bin\release\OgrePlatform.dll Release\
-copy %OgreSDKdir%\bin\release\Plugin_CgProgramManager.dll Release\
-copy %OgreSDKdir%\bin\release\Plugin_ParticleFX.dll Release\
-copy %OgreSDKdir%\bin\release\Plugin_OctreeSceneManager.dll Release\
-copy %OgreSDKdir%\bin\release\Plugin_BSPSceneManager.dll Release\
-copy %OgreSDKdir%\bin\release\RenderSystem_Direct3D9.dll Release\
-copy %OgreSDKdir%\bin\release\RenderSystem_GL.dll Release\
-copy %OgreSDKdir%\media\packs\OgreCore.zip Release\
+copy %OgreSDKdir%\bin\release\cg.dll ..\bin\Release\
+copy %OgreSDKdir%\bin\release\DevIL.dll ..\bin\Release\
+copy %OgreSDKdir%\bin\release\ILU.dll ..\bin\Release\
+copy %OgreSDKdir%\bin\release\ILUT.dll ..\bin\Release\
+copy %OgreSDKdir%\bin\release\zlib1.dll ..\bin\Release\
+copy %OgreSDKdir%\bin\release\OgreMain.dll ..\bin\Release\
+copy %OgreSDKdir%\bin\release\OgrePlatform.dll ..\bin\Release\
+copy %OgreSDKdir%\bin\release\Plugin_CgProgramManager.dll ..\bin\Release\
+copy %OgreSDKdir%\bin\release\Plugin_ParticleFX.dll ..\bin\Release\
+copy %OgreSDKdir%\bin\release\Plugin_OctreeSceneManager.dll ..\bin\Release\
+copy %OgreSDKdir%\bin\release\Plugin_BSPSceneManager.dll ..\bin\Release\
+copy %OgreSDKdir%\bin\release\RenderSystem_Direct3D9.dll ..\bin\Release\
+copy %OgreSDKdir%\bin\release\RenderSystem_GL.dll ..\bin\Release\
+copy %OgreSDKdir%\media\packs\OgreCore.zip ..\bin\Release\
