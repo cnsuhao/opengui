207950a8d2eca53ffe2b0815403debca55caad4d
zeroskill <zeroskill@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2006-11-28 23:35:24 +0000

    Removed unnecessary comment OpenGUI_Window.cpp Ticket: #107

--- trunk/OpenGUI/OpenGUI_Window.cpp
+++ trunk/OpenGUI/OpenGUI_Window.cpp
@@ -1,40 +1,6 @@
 #include "OpenGUI_Window.h"
 
 namespace OpenGUI {
-	/*
-	//############################################################################
-	class Widget_Name_ObjectProperty : public ObjectProperty {
-	public:
-	virtual const char* getAccessorName() {
-	return "Name";
-	}
-	//############################################################################
-	virtual void get( Object& objectRef, Value& valueOut ) {
-	try {
-	Widget &w = dynamic_cast<Widget &>( objectRef );
-	valueOut.setValue( w.getName() );
-	} catch ( std::bad_cast e ) {
-	OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
-	}
-	}
-	//############################################################################
-	virtual void set( Object& objectRef, Value& valueIn ) {
-	try {
-	Widget &w = dynamic_cast<Widget &>( objectRef );
-	w.setName( valueIn.getValueAsString() );
-	} catch ( std::bad_cast e ) {
-	OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
-	}
-	}
-	//############################################################################
-	virtual Value::ValueType getPropertyType() {
-	return Value::T_STRING;
-	}
-	}
-	gWidget_Name_ObjectProperty;
-	//############################################################################
-	//############################################################################
-	*/
 
 	class Window_ObjectAccessorList : public ObjectAccessorList {
 	public:
