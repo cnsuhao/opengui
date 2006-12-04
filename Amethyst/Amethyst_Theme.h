d19fd0869ee3f90d10c4afb61f53e33cdd289687
zeroskill <zeroskill@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2006-12-04 13:04:57 +0000

    Ticket: #111

---
+++ Amethyst/Amethyst_Theme.h
@@ -0,0 +1,13 @@
+#ifndef Amethyst_Theme_h__
+#define Amethyst_Theme_h__
+
+#include "OpenGUI.h"
+#include "Amethyst_Exports.h"
+
+namespace OpenGUI {
+	namespace Amethyst {
+
+	} // namespace Amethyst{
+} // namespace OpenGUI{
+
+#endif // Amethyst_Theme_h__
