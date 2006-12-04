78342a07f7b28314b4c9c848b57b38ab35cafcec
zeroskill <zeroskill@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2006-12-04 21:20:57 +0000

    Big chunk of theme system implemented Ticket: #111

--- trunk/Amethyst/Amethyst_Theme.h
+++ trunk/Amethyst/Amethyst_Theme.h
@@ -7,6 +7,50 @@
 namespace OpenGUI {
 	namespace Amethyst {
 
+		class AMETHYST_API StateImage {
+		public:
+			StateImage() {}
+			StateImage( ImageryPtr imagery ): mIsFace( false ), mImagery( imagery ) {}
+			StateImage( FacePtr face ): mIsFace( true ), mFace( face ) {}
+			~StateImage() {}
+			void draw( const FRect& rect, Brush& brush ) const {
+				if ( mIsFace )
+					brush.Image.drawFace( mFace, rect );
+				else
+					brush.Image.drawImage( mImagery, rect );
+			}
+		private:
+			bool mIsFace;
+			ImageryPtr mImagery;
+			FacePtr mFace;
+		};
+
+		class AMETHYST_API WidgetTheme {
+		public:
+			WidgetTheme() {}
+			~WidgetTheme() {}
+			void drawStateImage( const std::string& stateName, const FRect& rect, Brush& brush ) const {
+				( mStateImageMap.find( stateName ) )->second.draw( rect, brush );
+			}
+			typedef std::map<std::string, StateImage> StateImageMap;
+			StateImageMap mStateImageMap;
+
+			typedef std::map<std::string, Value> ValueMap;
+			ValueMap mValueMap;
+		};
+
+		class AMETHYST_API Theme {
+		public:
+			Theme() {}
+			~Theme() {}
+			void drawWidgetStateImage( const std::string& widgetName, const std::string& stateName, const FRect& rect, Brush& brush ) const {
+				( mWidgetThemeMap.find( widgetName ) )->second.drawStateImage( stateName, rect, brush );
+			}
+
+			typedef std::map<std::string, WidgetTheme> WidgetThemeMap;
+			WidgetThemeMap mWidgetThemeMap;
+		};
+
 	} // namespace Amethyst{
 } // namespace OpenGUI{
 
--- trunk/Amethyst/Amethyst_ThemeManager.cpp
+++ trunk/Amethyst/Amethyst_ThemeManager.cpp
@@ -4,8 +4,8 @@ namespace OpenGUI {
 
 	// This has to be in the OpenGUI namespace because of the template it comes from
 	template<> Amethyst::ThemeManager* Singleton<Amethyst::ThemeManager>::mptr_Singleton = 0;
-	
-	namespace Amethyst {		
+
+	namespace Amethyst {
 		//############################################################################
 		ThemeManager& ThemeManager::getSingleton( void ) {
 			assert( mptr_Singleton );
@@ -16,6 +16,61 @@ namespace OpenGUI {
 			return mptr_Singleton;
 		}
 		//############################################################################
-
+		ThemeManager::ThemeManager() {
+			/**/
+		}
+		//############################################################################
+		ThemeManager::~ThemeManager() {
+			/**/
+		}
+		//############################################################################
+		void ThemeManager::createTheme( const std::string& themeName, const Theme& theme ) {
+			ThemeMap::iterator iter = mThemeMap.find( themeName );
+			if ( iter == mThemeMap.end() )
+				mThemeMap[themeName] = theme;
+			else
+				OG_THROW( Exception::ERR_DUPLICATE_ITEM, "A theme with given name already exists: " + themeName, __FUNCTION__ );
+		}
+		//############################################################################
+		const Theme& ThemeManager::getTheme( const std::string& themeName ) {
+			ThemeMap::iterator iter = mThemeMap.find( themeName );
+			if ( iter != mThemeMap.end() )
+				return iter->second;
+			iter = mThemeMap.find( mDefaultTheme );
+			if ( iter != mThemeMap.end() )
+				return iter->second;
+			OG_THROW( Exception::ERR_ITEM_NOT_FOUND, "No valid default theme, and could not find requested theme: " + themeName, __FUNCTION__ );
+		}
+		//############################################################################
+		void ThemeManager::destroyTheme( const std::string& themeName ) {
+			ThemeMap::iterator iter = mThemeMap.find( themeName );
+			if ( iter == mThemeMap.end() )
+				OG_THROW( Exception::ERR_ITEM_NOT_FOUND, "Could not find requested theme: " + themeName, __FUNCTION__ );
+			else
+				mThemeMap.erase( iter );
+		}
+		//############################################################################
+		void ThemeManager::setDefaultThemeName( const std::string& themeName ) {
+			mDefaultTheme = themeName;
+		}
+		//############################################################################
+		const WidgetTheme& ThemeManager::getWidgetTheme( const std::string& themeName, const std::string& widgetName ) {
+			ThemeMap::iterator iter = mThemeMap.find( themeName );
+			if ( iter != mThemeMap.end() ) {
+				Theme& t = iter->second;
+				Theme::WidgetThemeMap::iterator i = t.mWidgetThemeMap.find( widgetName );
+				if ( i != t.mWidgetThemeMap.end() )
+					return i->second;
+			}
+			iter = mThemeMap.find( mDefaultTheme );
+			if ( iter != mThemeMap.end() ) {
+				Theme& t = iter->second;
+				Theme::WidgetThemeMap::iterator i = t.mWidgetThemeMap.find( widgetName );
+				if ( i != t.mWidgetThemeMap.end() )
+					return i->second;
+			}
+			OG_THROW( Exception::ERR_ITEM_NOT_FOUND, "Could not resolve WidgetTheme: " + themeName + ":" + widgetName, __FUNCTION__ );
+		}
+		//############################################################################
 	} // namespace Amethyst{
 } // namespace OpenGUI{
