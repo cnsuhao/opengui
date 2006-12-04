81dd6f2def3007af62557527ac29cc993c47654d
mmanyen <mmanyen@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2006-12-04 17:59:37 +0000

    First function w/o frames Ticket: #76

--- Amethyst/Amethyst_ScrollBar_H.cpp
+++ Amethyst/Amethyst_ScrollBar_H.cpp
@@ -7,6 +7,20 @@ namespace OpenGUI {
 		//! Called when cursor was pressed and released within this Control
 		void ScrollBar_H::onCursor_Click( Object* sender, Cursor_EventArgs& evtArgs ) {
 			// check for above/below the thumb
+			if(!m_rThumbRect.isInside(evtArgs.Position))
+			{
+				// click is not on the thumb, so we check for pageup/pagedown click
+				if(evtArgs.Position.x < m_rThumbRect.min.x)
+				{
+					// left of the thumb, page up
+					pageUp();
+				}
+				if(evtArgs.Position.x > m_rThumbRect.max.x)
+				{
+					// right of the thumb, page down
+					pageDown();
+				}
+			}
 
 			OpenGUI::Control::onCursor_Click( sender, evtArgs );
 		}
@@ -33,7 +47,31 @@ namespace OpenGUI {
 					invalidate();
 					grabCursorFocus();
 				}
+				else
+				{
+					// check for above/below the thumb
+					if(!m_rThumbRect.isInside(evtArgs.Position))
+					{
+						// click is not on the thumb, so we check for pageup/pagedown click
+						if(evtArgs.Position.x < m_rThumbRect.min.x)
+						{
+							// left of the thumb, page down
+							pageDown();
+							m_bMousePaging = true;
+							// need to set a timer for more paging on held mouse
+						}
+						if(evtArgs.Position.x > m_rThumbRect.max.x)
+						{
+							// right of the thumb, page up
+							pageUp();
+							m_bMousePaging = true;
+							// need to set a timer for more paging on held mouse
+						}
+					}
+				}
 			}
+
+			Widget::onCursor_Press(sender, evtArgs);
 		}
 		//! "Cursor_Release" event
 		void ScrollBar_H::onCursor_Release( Object* sender, Cursor_EventArgs& evtArgs ) {
@@ -44,6 +82,9 @@ namespace OpenGUI {
 			}
 			releaseCursorFocus();
 			m_bMouseDraggingThumb = false;
+			m_bMousePaging = false;
+
+			Widget::onCursor_Release(sender, evtArgs);
 		}
 
 		void ScrollBar_H::onCursor_Move( Object* sender, Cursor_EventArgs& evtArgs )
@@ -54,11 +95,17 @@ namespace OpenGUI {
 			// now if we are dragging the thumb, we need to check it out
 			if(m_bMouseDraggingThumb)
 			{
-				FVector2 translatedPoint = pointFromScreen(evtArgs.Position);
 				// gerenate the new "top" location
-				float x = evtArgs.Position.x - m_vMosueOffset.x;
+				float x = evtArgs.Position.x - m_vMosueOffset.x; // x pos is offset by the mouse offest set on mouse down
+				x -= getRect().min.x;
+				if(x>0)
+				{
+					//now scale the position based on the width of the control
+					mThumbPosValue = x / getRect().getWidth();
+				}
+				else 
+					mThumbPosValue = 0;
 
-				mThumbPosValue = x / mPageSize;
 				invalidate();
 			}
 		}
@@ -66,15 +113,24 @@ namespace OpenGUI {
 		ScrollBar_H::ScrollBar_H()
 		{
 			mThumbPosValue = 0.1;
-			mPageSize = 600.0f;
+			mCanvasSize = 800.0f;
+			mPageSize = 100.0f;
 			mLineSize = 12.0f;
-			mThumbImageOffsets = FRect(5,5,5,5);
+			mThumbImageOffsets = FRect(5,5,12,12);
 
 			m_bMouseDraggingThumb = false;
+			m_bMousePaging = false;
 		}
 
 		ScrollBar_H::~ScrollBar_H()
 		{
+		}
+
+		//! Sets the imagery for the BG.
+		void ScrollBar_H::setImagery( std::string imageryName)
+		{
+			invalidate();
+			mBGImage = ImageryManager::getSingleton().getImagery( imageryName );
 		}
 
 		//! Sets the imagery for the thumb.
@@ -99,22 +155,26 @@ namespace OpenGUI {
 		void ScrollBar_H::pageUp()
 		{
 			invalidate();
-			mThumbPosValue += mPageSize;
+			mThumbPosValue += (mPageSize / mCanvasSize);
+			_positionThumb();
 		}
 		void ScrollBar_H::pageDown()
 		{
 			invalidate();
-			mThumbPosValue -= mPageSize;
+			mThumbPosValue -= (mPageSize / mCanvasSize);
+			_positionThumb();
 		}
 		void ScrollBar_H::lineUp()
 		{
 			invalidate();
 			mThumbPosValue += mLineSize;
+			_positionThumb();
 		}
 		void ScrollBar_H::lineDown()
 		{
 			invalidate();
 			mThumbPosValue -= mLineSize;
+			_positionThumb();
 		}
 
 		void ScrollBar_H::_positionThumb()
@@ -127,7 +187,7 @@ namespace OpenGUI {
 			FVector2 widgetSize = thumbTrackRect.max - thumbTrackRect.min;
 			m_rThumbRect.min.x += (widgetSize.x * mThumbPosValue);
 
-			float thumbWidthFactor = widgetSize.x / mPageSize;
+			float thumbWidthFactor = widgetSize.x / mCanvasSize;
 			m_rThumbRect.max.x = m_rThumbRect.min.x + (widgetSize.x * thumbWidthFactor);
 			// thumb is positioned, not check to see if it is off the end and fix
 			float backCheck = m_rThumbRect.max.x - thumbTrackRect.max.x;
@@ -140,15 +200,14 @@ namespace OpenGUI {
 
 		void ScrollBar_H::onDraw( Object* sender, Draw_EventArgs& evtArgs )
 		{
-			Frame::onDraw(sender, evtArgs);
-
 			// now position and draw out stuff
 			_positionThumb();
 
+			if ( !mBGImage.isNull() ) {
+				evtArgs.brush.Image.drawImage( mBGImage, getRect() );
+			}
 			if ( !mThumbImage.isNull() ) {
-				Brush& b = evtArgs.brush;
-
-				b.Image.drawImage( mThumbImage, m_rThumbRect );
+				evtArgs.brush.Image.drawImage( mThumbImage, m_rThumbRect );
 			}
 		}
 
