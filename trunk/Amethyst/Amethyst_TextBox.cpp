1875bc9ab5435669c33825b87a8bb85413d78f16
mmanyen <mmanyen@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2006-12-05 17:20:44 +0000

    Starting up the TextBox Ticket: #11

---
+++ trunk/Amethyst/Amethyst_TextBox.cpp
@@ -0,0 +1,101 @@
+#include "Amethyst_TextBox.h"
+
+namespace OpenGUI {
+	namespace Amethyst {
+
+		//! Constructor
+		TextBox::TextBox() {
+//			if(gSimpleButton_ObjectAccessorList.getParent() == NULL)
+//				gSimpleButton_ObjectAccessorList.setParent(Widget::getAccessors());
+
+			mText = "";
+			m_alignh = TextAlignment::ALIGN_CENTER;
+			m_alignv = TextAlignment::ALIGN_CENTER;
+			mFont = Font( "pecot", 10 );
+		}
+
+		//! virtual Destructor
+		TextBox::~TextBox() {
+			/**/
+		}
+
+		//! Sets the normal button imagery.
+		void TextBox::setImagery( std::string imageryName ) {
+			invalidate();
+			mImageryPtr = ImageryManager::getSingleton().getImagery( imageryName );
+		}
+
+		//! Gets the normal button imagery.
+		std::string TextBox::getImagery() {
+			if ( !mImageryPtr.isNull() )
+				return mImageryPtr->getName();
+
+			return "";
+		}
+
+		//############################################################################
+		void TextBox::setAlignment( TextAlignment::Alignment h, TextAlignment::Alignment v ) {
+			m_alignh = h;
+			m_alignv = v;
+		}
+		//############################################################################
+		void TextBox::getAlignment( TextAlignment::Alignment &h, TextAlignment::Alignment &v ) {
+			h = m_alignh;
+			v = m_alignv;
+		}
+		//############################################################################
+		void TextBox::setAlignment( IVector2 &align ) {
+			TextAlignment::Alignment h = ( TextAlignment::Alignment ) align.x;
+			TextAlignment::Alignment v = ( TextAlignment::Alignment ) align.y;
+			setAlignment( h, v );
+		}
+		//############################################################################
+		void TextBox::getAlignment( IVector2 &align ) {
+			TextAlignment::Alignment h;
+			TextAlignment::Alignment v;
+			getAlignment( h, v );
+			align.x = h;
+			align.y = v;
+		}
+		//############################################################################
+
+		void TextBox::onDraw( Object* sender, Draw_EventArgs& evtArgs ) {
+			if ( !mImageryPtr.isNull() ) {
+				Brush& b = evtArgs.brush;
+				b.Image.drawImage( mImageryPtr, getRect() );
+				b.Text.drawTextArea( mText, getRect(), mFont, true, m_alignh, m_alignv );
+			}
+		}
+
+
+
+
+		Widget* TextBox::createTextBoxFactory() {
+			return new TextBox;
+		}
+
+		//! Called when cursor was pressed and released within this Control
+		void TextBox::onCursor_Click( Object* sender, Cursor_EventArgs& evtArgs ) {
+			OpenGUI::Control::onCursor_Click( sender, evtArgs );
+		}
+		//! Called when the cursor enters this Control
+		void TextBox::onCursor_Enter( Object* sender, Cursor_EventArgs& evtArgs ) {
+			OpenGUI::Control::onCursor_Enter( sender, evtArgs );
+		}
+		//! Called when the cursor leaves this Control
+		void TextBox::onCursor_Leave( Object* sender, Cursor_EventArgs& evtArgs ) {
+			OpenGUI::Control::onCursor_Leave( sender, evtArgs );
+		}
+
+		//! "Cursor_Press" event
+		void TextBox::onCursor_Press( Object* sender, Cursor_EventArgs& evtArgs ) {
+			Control::onCursor_Press( sender, evtArgs );
+		}
+		//! "Cursor_Release" event
+		void TextBox::onCursor_Release( Object* sender, Cursor_EventArgs& evtArgs ) {
+			Control::onCursor_Release( sender, evtArgs );
+		}
+
+
+	} // namespace Amethyst{
+} // namespace OpenGUI{
--- trunk/Amethyst/Amethyst_TextBox.h
+++ trunk/Amethyst/Amethyst_TextBox.h
@@ -1,5 +1,70 @@
 #ifndef Amethyst_TextBox_h__
 #define Amethyst_TextBox_h__
+#include "OpenGUI.h"
+#include "Amethyst_Exports.h"
+
+namespace OpenGUI {
+	namespace Amethyst {
+
+		//! encapsulates the basic GUI Keyboard input interaction
+		class AMETHYST_API TextBox : public Control {
+		public:
+			TextBox() ;
+			virtual ~TextBox() ;
+
+			//! Sets the BG imagery.
+			void setImagery( std::string imageryName );
+			//! Gets the BG imagery.
+			std::string getImagery();
+
+			void setText( std::string &nText )		{
+				mText = nText;
+			}
+			void setText( const char *p )			{
+				mText = p;
+			}
+			const std::string getText( void ) const	{
+				return mText;
+			}
+			void setFont( const Font& fnt )			{
+				mFont = fnt;
+			}
+			const Font& getFont() const				{
+				return mFont;
+			}
+
+			void setAlignment( TextAlignment::Alignment h, TextAlignment::Alignment v );
+			void getAlignment( TextAlignment::Alignment &h, TextAlignment::Alignment &v );
+			void setAlignment( IVector2 &align );
+			void getAlignment( IVector2 &align );
+
+			static Widget* createTextBoxFactory();
+
+		protected:
+			virtual void onDraw( Object* sender, Draw_EventArgs& evtArgs );
+
+			//! "Cursor_Click" event
+			virtual void onCursor_Click( Object* sender, Cursor_EventArgs& evtArgs );
+			//! "Cursor_Enter" event; invokes Targeted
+			virtual void onCursor_Enter( Object* sender, Cursor_EventArgs& evtArgs );
+			//! "Cursor_Leave" event; invokes UnTargeted
+			virtual void onCursor_Leave( Object* sender, Cursor_EventArgs& evtArgs );
+
+			//! "Cursor_Press" event
+			virtual void onCursor_Press( Object* sender, Cursor_EventArgs& evtArgs );
+			//! "Cursor_Release" event
+			virtual void onCursor_Release( Object* sender, Cursor_EventArgs& evtArgs );
+
+			ImageryPtr mImageryPtr;
+
+			std::string mText;
+			Font mFont;
+			TextAlignment::Alignment m_alignh;
+			TextAlignment::Alignment m_alignv;
+		};
+	} // namespace Amethyst{
+} // namespace OpenGUI{
+
 
 
 #endif // Amethyst_TextBox_h__
