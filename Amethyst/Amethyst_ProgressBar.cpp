11fd250894cc9011d8bfbc07e13c1440b1577bb7
zeroskill <zeroskill@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2006-12-09 10:07:25 +0000

    Progress on the ProgressBar ;) Ticket: #121

--- Amethyst/Amethyst_ProgressBar.cpp
+++ Amethyst/Amethyst_ProgressBar.cpp
@@ -1,223 +1,155 @@
 #include "Amethyst_ProgressBar.h"
+#include "OpenGUI_Macros.h"
 
 namespace OpenGUI {
 	namespace Amethyst {
-
-		class ProgressBar_Step_ObjectProperty : public ObjectProperty {
-		public:
-			virtual const char* getAccessorName() {
-				return "Step";
-			}
-			//############################################################################
-			virtual void get( Object& objectRef, Value& valueOut ) {
-				try {
-					ProgressBar & l = dynamic_cast<ProgressBar&>( objectRef );
-					valueOut.setValue( l.getStep() );
-				} catch ( std::bad_cast e ) {
-					OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
-				}
-			}
-			//############################################################################
-			virtual void set( Object& objectRef, Value& valueIn ) {
-				try {
-					ProgressBar& l = dynamic_cast<ProgressBar&>( objectRef );
-					l.setStep( valueIn.getValueAsFloat() );
-				} catch ( std::bad_cast e ) {
-					OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
-				}
-			}
-			//############################################################################
-			virtual Value::ValueType getPropertyType() {
-				return Value::T_FLOAT;
-			}
-		}
-		gProgressBar_Step_ObjectProperty;
-
-		class ProgressBar_CurrentValue_ObjectProperty : public ObjectProperty {
-		public:
-			virtual const char* getAccessorName() {
-				return "CurrentValue";
-			}
-			//############################################################################
-			virtual void get( Object& objectRef, Value& valueOut ) {
-				try {
-					ProgressBar & l = dynamic_cast<ProgressBar&>( objectRef );
-					valueOut.setValue( l.getCurrentValue() );
-				} catch ( std::bad_cast e ) {
-					OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
-				}
-			}
-			//############################################################################
-			virtual void set( Object& objectRef, Value& valueIn ) {
-				try {
-					ProgressBar& l = dynamic_cast<ProgressBar&>( objectRef );
-					l.setCurrentValue( valueIn.getValueAsFloat() );
-				} catch ( std::bad_cast e ) {
-					OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
-				}
-			}
-			//############################################################################
-			virtual Value::ValueType getPropertyType() {
-				return Value::T_FLOAT;
-			}
-		}
-		gProgressBar_CurrentValue_ObjectProperty;
-
-		class ProgressBar_DoneColor_ObjectProperty : public ObjectProperty {
-		public:
-			virtual const char* getAccessorName() {
-				return "DoneColor";
-			}
-			//############################################################################
-			virtual void get( Object& objectRef, Value& valueOut ) {
-				try {
-					ProgressBar & l = dynamic_cast<ProgressBar&>( objectRef );
-					valueOut.setValue( l.getDoneColor() );
-				} catch ( std::bad_cast e ) {
-					OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
-				}
-			}
-			//############################################################################
-			virtual void set( Object& objectRef, Value& valueIn ) {
-				try {
-					ProgressBar& l = dynamic_cast<ProgressBar&>( objectRef );
-					l.setDoneColor( valueIn.getValueAsColor() );
-				} catch ( std::bad_cast e ) {
-					OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
-				}
-			}
-			//############################################################################
-			virtual Value::ValueType getPropertyType() {
-				return Value::T_COLOR;
-			}
-		}
-		gProgressBar_DoneColor_ObjectProperty;
-		class ProgressBar_TodoColor_ObjectProperty : public ObjectProperty {
-		public:
-			virtual const char* getAccessorName() {
-				return "TodoColor";
-			}
-			//############################################################################
-			virtual void get( Object& objectRef, Value& valueOut ) {
-				try {
-					ProgressBar & l = dynamic_cast<ProgressBar&>( objectRef );
-					valueOut.setValue( l.getTodoColor() );
-				} catch ( std::bad_cast e ) {
-					OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
-				}
-			}
-			//############################################################################
-			virtual void set( Object& objectRef, Value& valueIn ) {
-				try {
-					ProgressBar& l = dynamic_cast<ProgressBar&>( objectRef );
-					l.setTodoColor( valueIn.getValueAsColor() );
-				} catch ( std::bad_cast e ) {
-					OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
-				}
-			}
-			//############################################################################
-			virtual Value::ValueType getPropertyType() {
-				return Value::T_COLOR;
-			}
-		}
-		gProgressBar_TodoColor_ObjectProperty;
-
-
-		// this is a goofy property, any access will cause the "step" function to be called
-		class ProgressBar_DoStep_ObjectProperty : public ObjectProperty {
-		public:
-			virtual const char* getAccessorName() {
-				return "DoStep";
-			}
-			//############################################################################
-			virtual void get( Object& objectRef, Value& valueOut ) {
-				try {
-					ProgressBar & l = dynamic_cast<ProgressBar&>( objectRef );
-					l.doStep();
-					valueOut.setValue( l.getCurrentValue() );
-				} catch ( std::bad_cast e ) {
-					OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
-				}
-			}
-			//############################################################################
-			virtual void set( Object& objectRef, Value& valueIn ) {
-				try {
-					ProgressBar& l = dynamic_cast<ProgressBar&>( objectRef );
-					l.doStep();
-				} catch ( std::bad_cast e ) {
-					OG_THROW( Exception::ERR_INVALIDPARAMS, "Bad Object Pointer", __FUNCTION__ );
-				}
-			}
-			//############################################################################
-			virtual Value::ValueType getPropertyType() {
-				return Value::T_FLOAT;
-			}
-		}
-		gProgressBar_DoStep_ObjectProperty;
-
+		//############################################################################
+		SimpleProperty_Font( PBProp_Font, "Font", ProgressBar, getFont, setFont );
+		SimpleProperty_Face( PBProp_BGFace, "BGFace", ProgressBar, getBGFace, setBGFace );
+		SimpleProperty_Face( PBProp_FillFace, "FillFace", ProgressBar, getFillFace, setFillFace );
+		SimpleProperty_Float( PBProp_StepSize, "Step_Size", ProgressBar, getStepSize, setStepSize );
+		SimpleProperty_Float( PBProp_Total, "Total", ProgressBar, getTotal, setTotal );
+		SimpleProperty_Float( PBProp_Value, "Value", ProgressBar, getValue, setValue );
+		SimpleProperty_Bool( PBProp_Vertical, "Vertical", ProgressBar, getVertical, setVertical );
+		SimpleProperty_Bool( PBProp_Inverted, "Inverted", ProgressBar, getInverted, setInverted );
+		SimpleProperty_Color( PBProp_FillColor, "FillColor", ProgressBar, getFillColor, setFillColor );
+		SimpleProperty_Color( PBProp_BGColor, "BGColor", ProgressBar, getBGColor, setBGColor );
+		//############################################################################
 		class ProgressBar_ObjectAccessorList : public ObjectAccessorList {
 		public:
 			ProgressBar_ObjectAccessorList() {
-				addAccessor( &gProgressBar_Step_ObjectProperty );
-				addAccessor( &gProgressBar_CurrentValue_ObjectProperty );
-				addAccessor( &gProgressBar_DoStep_ObjectProperty );
-				addAccessor( &gProgressBar_TodoColor_ObjectProperty);
-				addAccessor( &gProgressBar_DoneColor_ObjectProperty);
+				addAccessor( &PBProp_Font );
+				addAccessor( &PBProp_BGFace );
+				addAccessor( &PBProp_FillFace );
+				addAccessor( &PBProp_StepSize );
+				addAccessor( &PBProp_Total );
+				addAccessor( &PBProp_Value );
+				addAccessor( &PBProp_Vertical );
+				addAccessor( &PBProp_Inverted );
+				addAccessor( &PBProp_FillColor );
+				addAccessor( &PBProp_BGColor );
 			}
 			~ProgressBar_ObjectAccessorList() {}
 		}
 		gProgressBar_ObjectAccessorList;
-
-
-
-		ProgressBar::ProgressBar()
-		{
-			if(gProgressBar_ObjectAccessorList.getParent() == NULL)
-				gProgressBar_ObjectAccessorList.setParent(Widget::getAccessors());
-
-			mStep = 0.1f;
-			mCurrentValue = 0.0f;
-
-			m_cComplete = Color(0,0,1,1);
-			m_cTodo = Color(1,0,0,1);
+		//############################################################################
+		//############################################################################
+		ObjectAccessorList* ProgressBar::getAccessors() {
+			return &gProgressBar_ObjectAccessorList;
 		}
-
-		ProgressBar::~ProgressBar()
-		{
-		}
-
+		//############################################################################
 		Widget* ProgressBar::createProgressBarFactory() {
 			return new ProgressBar;
 		}
+		//############################################################################
+		ProgressBar::ProgressBar() {
+			if ( gProgressBar_ObjectAccessorList.getParent() == NULL )
+				gProgressBar_ObjectAccessorList.setParent( Control::getAccessors() );
 
-		void ProgressBar::onDraw( Object* sender, Draw_EventArgs& evtArgs )
-		{
+
+		}
+		//############################################################################
+		ProgressBar::~ProgressBar() {
+			/**/
+		}
+		//############################################################################
+		void ProgressBar::setFont( const Font& fnt ) {
+			mFont = fnt;
+			invalidate();
+		}
+		//############################################################################
+		const Font& ProgressBar::getFont() const {
+			return mFont;
+		}
+		//############################################################################
+		void ProgressBar::setTotal( float total ) {
+			mTotal = total;
+			invalidate();
+		}
+		//############################################################################
+		float ProgressBar::getTotal() {
+			return mTotal;
+		}
+		//############################################################################
+		void ProgressBar::setStepSize( float stepSize ) {
+			mStepSize = stepSize;
+			invalidate();
+		}
+		//############################################################################
+		float ProgressBar::getStepSize() const {
+			return mStepSize;
+		}
+		//############################################################################
+		void ProgressBar::doStep() {
+			OG_NYI;
+		}
+		//############################################################################
+		void ProgressBar::setValue( float newValue ) {
+			mValue = newValue;
+			invalidate();
+		}
+		//############################################################################
+		float ProgressBar::getValue() const {
+			return mValue;
+		}
+		//############################################################################
+		void ProgressBar::setBGColor( const Color &color ) {
+			mColorBG = color;
+			invalidate();
+		}
+		//############################################################################
+		const Color& ProgressBar::getBGColor() const {
+			return mColorBG;
+		}
+		//############################################################################
+		void ProgressBar::setFillColor( const Color &color ) {
+			mColorFG = color;
+			invalidate();
+		}
+		//############################################################################
+		const Color& ProgressBar::getFillColor() const {
+			return mColorFG;
+		}
+		//############################################################################
+		void ProgressBar::setBGFace( FacePtr facePtr ) {
+			mFaceBG = facePtr;
+			invalidate();
+		}
+		//############################################################################
+		FacePtr ProgressBar::getBGFace() const {
+			return mFaceBG;
+		}
+		//############################################################################
+		void ProgressBar::setFillFace( FacePtr facePtr ) {
+			mFaceFG = facePtr;
+			invalidate();
+		}
+		//############################################################################
+		FacePtr ProgressBar::getFillFace() const {
+			return mFaceFG;
+		}
+		//############################################################################
+		void ProgressBar::setVertical( bool vertical ) {
+			mVertical = vertical;
+			invalidate();
+		}
+		//############################################################################
+		bool ProgressBar::getVertical() const {
+			return mVertical;
+		}
+		//############################################################################
+		void ProgressBar::setInverted( bool inverted ) {
+			mInverted = inverted;
+			invalidate();
+		}
+		//############################################################################
+		bool ProgressBar::getInverted() const {
+			return mInverted;
+		}
+		//############################################################################
+		void ProgressBar::onDraw( Object* sender, Draw_EventArgs& evtArgs ) {
 			Brush& b = evtArgs.brush;
-			FRect complete, todo;
-
-			complete = getRect();
-			complete.setWidth(mCurrentValue * complete.getWidth());
-
-			todo = getRect();
-			todo.offset(FVector2(mCurrentValue * todo.getWidth(), 0.0f));
-			todo.setWidth((1.0f - mCurrentValue) * todo.getWidth());
-
-			b.pushColor(m_cComplete);
-			b.Primitive.drawRect(complete);
-			b.pop();
-
-			b.pushColor(m_cTodo);
-			b.Primitive.drawRect(todo);
-			b.pop();
-
-			char buf[16];
-			sprintf(buf, "%3.0f%%",mCurrentValue*100.0f);
-			std::string pctText = buf;
-			b.Text.drawTextArea( pctText, getRect(), mFont, false, TextAlignment(TextAlignment::ALIGN_CENTER) );
 		}
-
-		void ProgressBar::onResized( Object* sender, Resized_EventArgs& evtArgs )
-		{
-		}
-	}
-}
+		//############################################################################
+	} //namespace Amethyst {
+} // namespace OpenGUI {
