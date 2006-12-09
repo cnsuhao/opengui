b46d4e3f7194918f21946c76fcd096811baa3ddf
zeroskill <zeroskill@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2006-12-09 04:05:14 +0000

    * removed Frame (no longer needed) Ticket: #121

--- trunk/Amethyst/Amethyst_ListBox.cpp
+++ trunk/Amethyst/Amethyst_ListBox.cpp
@@ -3,35 +3,6 @@
 namespace OpenGUI {
 	namespace Amethyst {
 
-		ListBox::ListBox()
-		{
-		}
-
-		ListBox::~ListBox()
-		{
-		}
-
-
-		Widget* ListBox::CreateListBoxFactory()
-		{
-			return new ListBox;
-		}
-
-		void ListBox::onDraw( Object* sender, Draw_EventArgs& evtArgs )
-		{
-			Frame::onDraw( sender, evtArgs );
-		}
-
-		void ListBox::onResized( Object* sender, Resized_EventArgs& evtArgs )
-		{
-			//reset the position and size of our children
-
-			Frame::onResized(sender, evtArgs);
-		}
-
-		void ListBox::onMoved( Object* sender, Moved_EventArgs& evtArgs )
-		{
-			Frame::onMoved(sender, evtArgs);
-		}
-	}
-}
+		
+	} // namespace Amethyst {
+} // namespace OpenGUI {
--- trunk/Amethyst/Amethyst_ListBox.h
+++ trunk/Amethyst/Amethyst_ListBox.h
@@ -3,24 +3,10 @@
 
 #include "OpenGUI.h"
 #include "Amethyst_Exports.h"
-#include "Amethyst_Frame.h"
+
 
 namespace OpenGUI {
 	namespace Amethyst {
-
-		class AMETHYST_API ListBox: public Frame {
-		public:
-			ListBox();
-			virtual ~ListBox();
-
-
-			static Widget* CreateListBoxFactory();
-
-		protected:
-			virtual void onDraw( Object* sender, Draw_EventArgs& evtArgs );
-			virtual void onResized( Object* sender, Resized_EventArgs& evtArgs );
-			virtual void onMoved( Object* sender, Moved_EventArgs& evtArgs );
-		};
 
 	} // namespace Amethyst{
 } // namespace OpenGUI{
