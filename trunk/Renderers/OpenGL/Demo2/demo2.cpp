7fedfd3857bc7522b75c2d0c4f8cb14a94f3038d
zeroskill <zeroskill@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2006-12-15 17:51:10 +0000

    Overwriting Demo2 with Demo4

--- trunk/Renderers/OpenGL/Demo2/demo2.cpp
+++ trunk/Renderers/OpenGL/Demo2/demo2.cpp
@@ -3,22 +3,6 @@
 // See LICENSE.TXT for details
 
 #include "../DemoAppFramework/DemoAppFrameWork.h"
-
-#include "../Amethyst/Amethyst_Label.h"
-#include "../Amethyst/Amethyst_StaticImage.h"
-#include "../Amethyst/Amethyst_Button.h"
-#include "../Amethyst/Amethyst_CheckBox.h"
-#include "../Amethyst/Amethyst_RadioButton.h"
-#include "../Amethyst/Amethyst_ProgressBar.h"
-#include "../Amethyst/Amethyst_Panel.h"
-#include "../Amethyst/Amethyst_Frame.h"
-#include "../Amethyst/Amethyst_ListBox.h"
-#include "../Amethyst/Amethyst_ScrollBar_H.h"
-#include "../Amethyst/Amethyst_ScrollBar_V.h"
-#include "../Amethyst/Amethyst_TextBox.h"
-
-#include "OpenGUI.h"
-#include "OpenGUI_EventHandler.h"
 #include "Renderer_OpenGL.h"
 
 class Demo2App : public DemoApp {
@@ -31,286 +15,224 @@ public:
 	}
 	virtual void mousePositionCallback( int x, int y );
 	virtual void mouseButtonCallback( int button, int action );
-	virtual void keyCallback( int key, int action );
-
 private:
-	//OpenGUI::Widgets::TextLabel* mLabel;
+	OpenGUI::Widget* mLabel;
 	OpenGUI::TimerPtr mTimer;
 	OpenGUI::Screen* mScreen;
 };
 
 using namespace OpenGUI;
 
-Amethyst::ProgressBar *g_pProgressBar = NULL;
-Amethyst::Panel *g_pPanel = NULL;
 
-void buttonCallback( Object *pObj , EventArgs *pEvent )
-{
-//	g_pProgressBar->doStep();
 
-	g_pPanel->Scroll(FVector2(-3,-3));
-}
+
 
 
 void Demo2App::preRun() {
-	XMLParser::getSingleton().LoadFromFile("metal.xml");
-	XMLParser::getSingleton().LoadFromFile("BlueGui.xml");
-	XMLParser::getSingleton().LoadFromFile("Generic.xml");
-	XMLParser::getSingleton().LoadFromFile("demo2.xml");
-	XMLParser::getSingleton().LoadFromFile("Amethyst.xml");
+	hideSystemCursor();
+	XMLParser::getSingleton().LoadFromFile( "demo2.xml" );
 
-	mScreen = ScreenManager::getSingleton().createScreen( "MainScreen", FVector2( 800, 600 ) );
 	Viewport* v = (static_cast<Renderer_OpenGL*>(Renderer::getSingletonPtr()))->getDefaultViewport();
-	mScreen->setViewport(v);
+	mScreen = ScreenManager::getSingleton().createScreen( "MainScreen", FVector2( 800, 600 ), v );
 
-	CursorPtr cursorPtr = CursorManager::getSingleton().CreateDefinedCursor("MetalCursor");
-	mScreen->setCursor(cursorPtr);
+	CursorPtr cursorPtr = CursorManager::getSingleton().CreateDefinedCursor( "Arrow" );
+	mScreen->setCursor( cursorPtr );
 	mScreen->enableCursor();
 
-	Amethyst::Label* headerText = new Amethyst::Label();
+
+	FacePtr fptr;
+	fptr = ImageryManager::getSingleton().getFace("GenericFrame");
+
+
+	Widget* w;
+	// Application label in the upper left
+	w = WidgetManager::getSingleton().CreateDefinedWidget("BrassBalls:Label");
+	w->setProperty("Top", Value(0.0f));
+	w->setProperty("Left", Value(0.0f));
+	w->setProperty("Width", Value(250.0f));
+	w->setProperty("Height", Value(25.f));
+	w->setProperty("Text", Value("Amethyst Regression Test Bed"));
+	mScreen->Children.add_front(w, true);
+
+
+	w = WidgetManager::getSingleton().CreateDefinedWidget("BrassBalls:Button");
+	w->setProperty("Top", Value(50.0f));
+	w->setProperty("Left", Value(50.0f));
+	w->setProperty("Width", Value(55.0f));
+	w->setProperty("Height", Value(30.f));
+	w->setProperty("Text",Value("Test"));
+	mScreen->Children.add_front(w, true);
+
+
+	w = WidgetManager::getSingleton().CreateDefinedWidget("BrassBalls:CheckBox");
+	w->setProperty("Top", Value(150.0f));
+	w->setProperty("Left", Value(150.0f));
+	w->setProperty("Width", Value(15.0f));
+	w->setProperty("Height", Value(15.0f));
+	mScreen->Children.add_front(w, true);
+	w = WidgetManager::getSingleton().CreateDefinedWidget("BrassBalls:CheckBox");
+	w->setProperty("Top", Value(170.0f));
+	w->setProperty("Left", Value(150.0f));
+	w->setProperty("Width", Value(15.0f));
+	w->setProperty("Height", Value(15.f));
+	mScreen->Children.add_front(w, true);
+
+	w = WidgetManager::getSingleton().CreateDefinedWidget("BrassBalls:RadioButton");
+	w->setProperty("Top", Value(150.0f));
+	w->setProperty("Left", Value(190.0f));
+	w->setProperty("Width", Value(15.0f));
+	w->setProperty("Height", Value(15.f));
+	w->setProperty("Group", Value("One"));
+	mScreen->Children.add_front(w, true);
+
+	w = WidgetManager::getSingleton().CreateDefinedWidget("BrassBalls:RadioButton");
+	w->setProperty("Top", Value(150.0f));
+	w->setProperty("Left", Value(210.0f));
+	w->setProperty("Width", Value(15.0f));
+	w->setProperty("Height", Value(15.f));
+	w->setProperty("Group", Value("One"));
+	mScreen->Children.add_front(w, true);
+
+	w = WidgetManager::getSingleton().CreateDefinedWidget("BrassBalls:RadioButton");
+	w->setProperty("Top", Value(170.0f));
+	w->setProperty("Left", Value(190.0f));
+	w->setProperty("Width", Value(15.0f));
+	w->setProperty("Height", Value(15.f));
+	w->setProperty("Group", Value("Two"));
+	mScreen->Children.add_front(w, true);
+
+	w = WidgetManager::getSingleton().CreateDefinedWidget("BrassBalls:RadioButton");
+	w->setProperty("Top", Value(170.0f));
+	w->setProperty("Left", Value(210.0f));
+	w->setProperty("Width", Value(15.0f));
+	w->setProperty("Height", Value(15.f));
+	w->setProperty("Group", Value("Two"));
+	mScreen->Children.add_front(w, true);
+
+	// Add a progress bar
+	w = WidgetManager::getSingleton().CreateDefinedWidget("BrassBalls:ProgressBar");
+	w->setProperty("Top", Value(100.0f));
+	w->setProperty("Left", Value(400.0f));
+	w->setProperty("Width", Value(150.0f));
+	w->setProperty("Height", Value(20.f));
+	mScreen->Children.add_front(w, true);
+
+	// Add a horizontal scroll bar
+	w = WidgetManager::getSingleton().CreateDefinedWidget("BrassBalls:ScrollBar_H");
+	w->setProperty("Top", Value(130.0f));
+	w->setProperty("Left", Value(400.0f));
+	w->setProperty("Width", Value(150.0f));
+	w->setProperty("Height", Value(20.f));
+	mScreen->Children.add_front(w, true);
+
+	// Add a vertical scroll bar
+	w = WidgetManager::getSingleton().CreateDefinedWidget("BrassBalls:ScrollBar_V");
+	w->setProperty("Top", Value(130.0f));
+	w->setProperty("Left", Value(700.0f));
+	w->setProperty("Width", Value(20.f));
+	w->setProperty("Height", Value(150.0f));
+	mScreen->Children.add_front(w, true);
+
+	// Add a panel
+	w = WidgetManager::getSingleton().CreateDefinedWidget("BrassBalls:Panel");
+	w->setProperty("Top", Value(100.0f));
+	w->setProperty("Left", Value(300.0f));
+	w->setProperty("Width", Value(40.0f));
+	w->setProperty("Height", Value(20.f));
+	mScreen->Children.add_front(w, true);
+
+	// Add a window
+	w = WidgetManager::getSingleton().CreateDefinedWidget("BrassBalls:Window");
+	w->setProperty("Top", Value(200.0f));
+	w->setProperty("Left", Value(200.0f));
+	w->setProperty("Width", Value(150.0f));
+	w->setProperty("Height", Value(150.f));
+	CursorPtr sizer_NWSE = CursorManager::getSingleton().CreateDefinedCursor( "Sizer_NWSE" );
+	CursorPtr sizer_NESW = CursorManager::getSingleton().CreateDefinedCursor( "Sizer_NESW" );
+	w->setProperty("CursorUL", Value(sizer_NWSE));
+	w->setProperty("CursorLR", Value(sizer_NWSE));
+	w->setProperty("CursorLL", Value(sizer_NESW));
+	w->setProperty("CursorUR", Value(sizer_NESW));
+	mScreen->Children.add_front(w, true);
+
+
+	// Show the full source Imageset in the lower right
+	w = WidgetManager::getSingleton().CreateRawWidget("StaticImage","Amethyst");
+	w->setProperty("Top", Value(600.0f - 256.0f));
+	w->setProperty("Left", Value(800.0f - 256.0f));
+	w->setProperty("Width", Value(256.0f));
+	w->setProperty("Height", Value(256.0f));
+	w->setProperty("Native_Scale", Value(false));
+	w->setProperty("Imagery", Value(ImageryManager::getSingleton().getImagery("BrassBalls.png:All")));
+	mScreen->Children.add_back(w, true);
+
+
+	//Widget* label = WidgetManager::CreateDefinedWidget("Label");
+	//label->setProperty("Text","This is some text");
+	//label->setProperty("Left", 0.0f);
+	//label->setProperty("Top", 0.0f);
+	//mScreen->Children.add_front(label,true);
+	/*
+	SimpleText* headerText = new SimpleText();
 	headerText->setName( "Header" );
 	headerText->setFont( Font( "pecot", 40 ) );
-	headerText->setText( "OpenGUI Amethyst Demo" );
+	headerText->setText( "OpenGUI Demo 1" );
 	headerText->setTop( 0 );
 	headerText->setLeft( 0 );
 	headerText->setWidth( 400 );
 	headerText->setHeight( 60 );
-	headerText->setAlignment(TextAlignment::ALIGN_LEFT, TextAlignment::ALIGN_TOP);
+	headerText->setAlignment( TextAlignment::ALIGN_LEFT, TextAlignment::ALIGN_TOP );
 	mScreen->Children.add_back( headerText, true );
 
-#if 0
-	Amethyst::SimpleButton* testButton = new Amethyst::SimpleButton();
-	testButton->setName("SampleMetalButton");
-	testButton->setImagery("MetalButtonNormal");
-	testButton->setImageryMouseOver("MetalButtonHover");
-	testButton->setImageryPressed("MetalButtonPress");
-	testButton->setImageryDisabled("MetalButtonDisabled");
-	testButton->setTop(70);
-	testButton->setLeft(40);
-	testButton->setWidth(300);
-	testButton->setHeight(60);
-	testButton->setText("Button!");
-	mScreen->Children.add_back( testButton, true );
-#else
-	// create a container for the button
-	Amethyst::Panel *pButtonPanel = (Amethyst::Panel *) Amethyst::Panel::CreatePanelFactory();
-	pButtonPanel->setTop(70);
-	pButtonPanel->setLeft(40);
-	pButtonPanel->setHeight(60);
-	pButtonPanel->setWidth(200);
-	mScreen->Children.add_back(pButtonPanel, true);
+	SimpleText* statText = new SimpleText();
+	mStatText = statText;
+	statText->setName( "Stats" );
+	statText->setFont( Font( "pecot", 20 ) );
+	statText->setText( "Stats on the way!\nPreload\n1234567890." );
+	statText->setTop( 0 );
+	statText->setLeft( 500 );
+	statText->setWidth( 400 );
+	statText->setHeight( 60 );
+	statText->setAlignment( TextAlignment::ALIGN_LEFT, TextAlignment::ALIGN_TOP );
+	mScreen->Children.add_back( statText, true );
 
-	Amethyst::SimpleButton* testButton = new Amethyst::SimpleButton();
-	testButton->setName("SampleBlueButton");
-	testButton->setImagery("BlueGuiButtonNormal");
-	testButton->setImageryMouseOver("BlueGuiButtonHover");
-	testButton->setImageryPressed("BlueGuiButtonPress");
-	testButton->setImageryDisabled("BlueGuiButtonDisabled");
-	testButton->setTop(10);
-	testButton->setLeft(30);
-	testButton->setWidth(32);
-	testButton->setHeight(32);
-	testButton->setText("Button!");
-	pButtonPanel->Children.add_back( testButton, true );
-#endif
-
-	testButton->getEvents().createEvent("Activate");
-	testButton->getEvents()["Activate"].add( new EventCallback((EventCallback::EventCallbackFunc *)&buttonCallback) );
-
-	Amethyst::CheckBox* testCheck = new Amethyst::CheckBox();
-	testCheck->setName("SampleMetalCheck");
-	testCheck->setImagery("MetalButtonNormal");
-	testCheck->setImageryMouseOver("MetalButtonHover");
-	testCheck->setImageryPressed("MetalButtonPress");
-	testCheck->setImageryDisabled("MetalButtonDisabled");
-	testCheck->setSelImagery("MetalButtonNormalX");
-	testCheck->setSelImageryMouseOver("MetalButtonHoverX");
-	testCheck->setSelImageryPressed("MetalButtonPressX");
-	testCheck->setSelImageryDisabled("MetalButtonDisabledX");
-	testCheck->setTop(70);
-	testCheck->setLeft(400);
-	testCheck->setWidth(60);
-	testCheck->setHeight(60);
-	mScreen->Children.add_back( testCheck, true );
-
-	testCheck->getEvents().createEvent("Activate");
-	testCheck->getEvents()["Activate"].add( new EventCallback((EventCallback::EventCallbackFunc *)&buttonCallback) );
+	*/
 
 
-	Amethyst::RadioButton* testRadio1 = new Amethyst::RadioButton();
-	testRadio1->setName("SampleMetalCheckRadio1");
-	testRadio1->setImagery("MetalButtonNormal");
-	testRadio1->setImageryMouseOver("MetalButtonHover");
-	testRadio1->setImageryPressed("MetalButtonPress");
-	testRadio1->setImageryDisabled("MetalButtonDisabled");
-	testRadio1->setSelImagery("MetalButtonNormalX");
-	testRadio1->setSelImageryMouseOver("MetalButtonHoverX");
-	testRadio1->setSelImageryPressed("MetalButtonPressX");
-	testRadio1->setSelImageryDisabled("MetalButtonDisabledX");
-	testRadio1->setTop(150);
-	testRadio1->setLeft(40);
-	testRadio1->setWidth(60);
-	testRadio1->setHeight(60);
-	testRadio1->setGroup("testGroup");
-	mScreen->Children.add_back( testRadio1, true );
-
-	testRadio1->getEvents().createEvent("Activate");
-	testRadio1->getEvents()["Activate"].add( new EventCallback((EventCallback::EventCallbackFunc *)&buttonCallback) );
-
-	Amethyst::RadioButton* testRadio2 = new Amethyst::RadioButton();
-	testRadio2->setName("SampleMetalCheckRadio2");
-	testRadio2->setImagery("MetalButtonNormal");
-	testRadio2->setImageryMouseOver("MetalButtonHover");
-	testRadio2->setImageryPressed("MetalButtonPress");
-	testRadio2->setImageryDisabled("MetalButtonDisabled");
-	testRadio2->setSelImagery("MetalButtonNormalX");
-	testRadio2->setSelImageryMouseOver("MetalButtonHoverX");
-	testRadio2->setSelImageryPressed("MetalButtonPressX");
-	testRadio2->setSelImageryDisabled("MetalButtonDisabledX");
-	testRadio2->setTop(220);
-	testRadio2->setLeft(40);
-	testRadio2->setWidth(60);
-	testRadio2->setHeight(60);
-	testRadio2->setGroup("testGroup");
-	mScreen->Children.add_back( testRadio2, true );
-
-	testRadio2->getEvents().createEvent("Activate");
-	testRadio2->getEvents()["Activate"].add( new EventCallback((EventCallback::EventCallbackFunc *)&buttonCallback) );
-
-	Amethyst::RadioButton* testRadio3 = new Amethyst::RadioButton();
-	testRadio3->setName("SampleMetalCheckRadio3");
-	testRadio3->setImagery("MetalButtonNormal");
-	testRadio3->setImageryMouseOver("MetalButtonHover");
-	testRadio3->setImageryPressed("MetalButtonPress");
-	testRadio3->setImageryDisabled("MetalButtonDisabled");
-	testRadio3->setSelImagery("MetalButtonNormalX");
-	testRadio3->setSelImageryMouseOver("MetalButtonHoverX");
-	testRadio3->setSelImageryPressed("MetalButtonPressX");
-	testRadio3->setSelImageryDisabled("MetalButtonDisabledX");
-	testRadio3->setTop(290);
-	testRadio3->setLeft(40);
-	testRadio3->setWidth(60);
-	testRadio3->setHeight(60);
-	testRadio3->setGroup("testGroup");
-	mScreen->Children.add_back( testRadio3, true );
-
-	testRadio3->getEvents().createEvent("Activate");
-	testRadio3->getEvents()["Activate"].add( new EventCallback((EventCallback::EventCallbackFunc *)&buttonCallback) );
-
-	Amethyst::ProgressBar *bar = new Amethyst::ProgressBar;
-	bar->setTop(290);
-	bar->setLeft(110);
-	bar->setHeight(60);
-	bar->setWidth(400);
-	bar->setFont(Font( "pecot", 20 ));
-	mScreen->Children.add_back(bar, true);
-
-	g_pProgressBar = bar;
-
-	Amethyst::Panel *pPanel = (Amethyst::Panel *) Amethyst::Panel::CreatePanelFactory();
-	pPanel->setTop(220);
-	pPanel->setLeft(110);
-	pPanel->setHeight(53);
-	pPanel->setWidth(51);
-	mScreen->Children.add_back(pPanel, true);
-	g_pPanel = pPanel;
-
-	Amethyst::StaticImage *pStaticImage = (Amethyst::StaticImage *) Amethyst::StaticImage::CreateStaticImageFactory();
-	pStaticImage->setTop(0);
-	pStaticImage->setLeft(0);
-	pStaticImage->setHeight(106);
-	pStaticImage->setWidth(103);
-	pStaticImage->setImagery("PieFace");
-	pPanel->Children.add_back(pStaticImage, true);
-
-	Amethyst::Panel *pPanelFrame = (Amethyst::Panel *) Amethyst::Panel::CreatePanelFactory();
-	pPanelFrame->setTop(430);
-	pPanelFrame->setLeft(5);
-	pPanelFrame->setHeight(100);
-	pPanelFrame->setWidth(200);
-	mScreen->Children.add_back(pPanelFrame, true);
 
 
-	Amethyst::Frame *pFrame = (Amethyst::Frame *) Amethyst::Frame::CreateFrameFactory();
-	pFrame->setTop(25);
-	pFrame->setLeft(25);
-	pFrame->setHeight(50);
-	pFrame->setWidth(150);
-#if 0
-	pFrame->setImagery("FrameUL", Amethyst::Frame::UL);
-	pFrame->setImagery("FrameUM", Amethyst::Frame::UM);
-	pFrame->setImagery("FrameUR", Amethyst::Frame::UR);
-	pFrame->setImagery("FrameML", Amethyst::Frame::ML);
-	pFrame->setImagery("FrameMM", Amethyst::Frame::MM);
-//	pFrame->setImagery("PieFace", Amethyst::Frame::MM);
-	pFrame->setImagery("FrameMR", Amethyst::Frame::MR);
-	pFrame->setImagery("FrameLL", Amethyst::Frame::LL);
-	pFrame->setImagery("FrameLM", Amethyst::Frame::LM);
-	pFrame->setImagery("FrameLR", Amethyst::Frame::LR);
-#else
-	pFrame->setImagery("GenericFrameUL", Amethyst::Frame::UL);
-	pFrame->setImagery("GenericFrameUM", Amethyst::Frame::UM);
-	pFrame->setImagery("GenericFrameUR", Amethyst::Frame::UR);
-	pFrame->setImagery("GenericFrameML", Amethyst::Frame::ML);
-	pFrame->setImagery("GenericFrameMM", Amethyst::Frame::MM);
-	pFrame->setImagery("GenericFrameMR", Amethyst::Frame::MR);
-	pFrame->setImagery("GenericFrameLL", Amethyst::Frame::LL);
-	pFrame->setImagery("GenericFrameLM", Amethyst::Frame::LM);
-	pFrame->setImagery("GenericFrameLR", Amethyst::Frame::LR);
-#endif
-	pPanelFrame->Children.add_back(pFrame, true);
-
-	Amethyst::StaticImage *pBGImage = (Amethyst::StaticImage *) Amethyst::StaticImage::CreateStaticImageFactory();
-	pBGImage->setTop(0);
-	pBGImage->setLeft(0);
-	pBGImage->setHeight(100);
-	pBGImage->setWidth(200);
-	pBGImage->setImagery("PieFace");
-	pPanelFrame->Children.add_back(pBGImage, true);
-
-
-	Amethyst::ScrollBar_H *pSH = (Amethyst::ScrollBar_H *) Amethyst::ScrollBar_H::CreateScrollBar_HFactory();
-	pSH->setTop(430);
-	pSH->setLeft(370);
-	pSH->setHeight(35);
-	pSH->setWidth(150);
-	pSH->setImagery("GenericHScroll1");
-	pSH->setThumbImagery("GenericHScrollThumb1");
-	mScreen->Children.add_back(pSH, true);
-
-	Amethyst::ScrollBar_V *pSV = (Amethyst::ScrollBar_V *) Amethyst::ScrollBar_V::CreateScrollBar_VFactory();
-	pSV->setTop(430);
-	pSV->setLeft(550);
-	pSV->setHeight(150);
-	pSV->setWidth(35);
-	pSV->setImagery("GenericVScroll1");
-	pSV->setThumbImagery("GenericVScrollThumb1");
-	mScreen->Children.add_back(pSV, true);
-
-
-	Amethyst::TextBox *pBox = (Amethyst::TextBox *) Amethyst::TextBox::createTextBoxFactory();
-	pBox->setTop(400);
-	pBox->setLeft(370);
-	pBox->setHeight(35);
-	pBox->setWidth(150);
-	pBox->setImagery("GenericHScroll1");
-	mScreen->Children.add_back(pBox, true);
-
+	mTimer = OpenGUI::TimerManager::getSingleton().getTimer();
 }
-
-
-int count = 0;
 void Demo2App::perframeRun() {
-	if(++count >= 100)
-	{
-		count = 0;
-		g_pProgressBar->doStep();
+
+
+	if ( OpenGUI::System::getSingletonPtr() ) {
+
+
+		if ( mTimer->getMilliseconds() > 500 ) {
+			mTimer->reset();
+			float FPS = ScreenManager::getSingleton().statGetFPS();
+			std::stringstream ss;
+			ss << "Update Time: " << mScreen->statsGetUpdateTime();
+			ss << "\n";
+			ss << "FPS: " << FPS;
+
+			//(( SimpleText* )mStatText )->setText( ss.str() );
+			//MyWnd* wnd = ( MyWnd* )mScreen->Children.getWidget( "MyWnd" );
+			/*if ( wnd ) {
+			Examples::Tachometer* wndTach = ( Examples::Tachometer* ) wnd->Children.getWidget( "wndTach" );
+			wndTach->setNeedleValue( FPS );
+			//wnd->setWidth(Math::Ceil(FPS));
+			//wndTach->setAlpha(FPS/300.0f);
+			//wnd->setAlpha(FPS/300.0f);
+			}*/
+		}
 	}
 }
 void Demo2App::mousePositionCallback( int x, int y ) {
-	int sx,sy;
-	getWindowSize(sx,sy);
-	mScreen->injectCursorPosition_Percent(((float)x) / ((float)sx),((float)y) / ((float)sy) );
+	int sx, sy;
+	getWindowSize( sx, sy );
+	mScreen->injectCursorPosition_Percent((( float )x ) / (( float )sx ), (( float )y ) / (( float )sy ) );
 }
 void Demo2App::mouseButtonCallback( int button, int action ) {
 	if ( button == 0 ) {
@@ -321,13 +243,6 @@ void Demo2App::mouseButtonCallback( int button, int action ) {
 	}
 }
 
-void Demo2App::keyCallback( int key, int action )
-{
-	if(action)
-		mScreen->injectCharacter(key);
-}
-
-
 int main( void ) {
 	Demo2App app;
 
@@ -335,7 +250,5 @@ int main( void ) {
 
 	return 0;
 }
-
-
 
 
