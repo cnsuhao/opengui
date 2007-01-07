4757b10688e95f7c943277a4b77bd97b4be79a10
zeroskill <zeroskill@2828181b-9a0d-0410-a8fe-e25cbdd05f59>
2007-01-07 06:00:52 +0000

    this is where it all begins Ticket: #128

---
+++ trunk/OpenGUI/OpenGUI_String.h
@@ -0,0 +1,15 @@
+// OpenGUI (http://opengui.sourceforge.net)
+// This source code is release under the BSD License
+// See LICENSE.TXT for details
+
+#ifndef E5AE9E12_AF34_48ff_B669_2802A0C3DC0A
+#define E5AE9E12_AF34_48ff_B669_2802A0C3DC0A
+
+#include "OpenGUI_PreRequisites.h"
+
+namespace OpenGUI{
+	typedef char Char;
+	typedef std::string String;
+} // namespace OpenGUI{
+
+#endif // E5AE9E12_AF34_48ff_B669_2802A0C3DC0A
